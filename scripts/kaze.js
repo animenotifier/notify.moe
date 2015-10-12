a9b8980f366f3b8a908d209180192b7ab69784d4
Eduard Urbach <e.urbach@gmail.com>
2015-10-12 13:05:56 +0900

    Added feedback page

--- scripts/kaze.js
+++ scripts/kaze.js
@@ -1,9 +1,12 @@
 var kaze = {};
 
-kaze.fadeSpeed = 250;
+kaze.fadeSpeed = 150;
 kaze.$navigation = $('#navigation');
 kaze.$container = $('#container');
 kaze.$content = $('#content');
+kaze.originalURL = window.location.pathname;
+kaze.currentURL = kaze.originalURL;
+kaze.lastRequest = null;
 
 kaze.ajaxifyLinks = function() {
 	$('.ajax').each(function() {
@@ -17,19 +20,15 @@ kaze.ajaxifyLinks = function() {
         if(url === window.location.pathname)
             return;
 
-        history.pushState(null, null, url);
-
         if(kaze.$navigation.offset().top < 0)
             kaze.scrollToElement(kaze.$navigation);
 
-        $.get('/_' + url, function(response) {
-            kaze.fadeContent(kaze.$content, response);
-        });
+        kaze.loadURL(url, true);
 	});
 };
 
 kaze.fadeContent = function($content, response) {
-	$content.stop().fadeOut(kaze.fadeSpeed, function() {
+	$content.promise().done(function() {
 		$content.html(response).fadeIn(kaze.fadeSpeed, function() {
 			kaze.ajaxifyLinks();
 		});
@@ -42,6 +41,60 @@ kaze.scrollToElement = function(element, time) {
     kaze.$container.animate({
         scrollTop: kaze.$container.scrollTop() + element.offset().top
     }, time);
-}
+};
 
+kaze.loadURL = function(url, addToHistory) {
+	if(kaze.lastRequest) {
+		kaze.lastRequest.abort();
+		kaze.lastRequest = null;
+	}
+
+	kaze.currentURL = url;
+
+	if(addToHistory) {
+		console.log('Push history: ' + url);
+		history.pushState(url, null, url);
+	}
+
+	kaze.$content.stop().fadeOut(kaze.fadeSpeed);
+
+	kaze.lastRequest = $.get('/_' + url, function(response) {
+		kaze.lastRequest = null;
+		kaze.fadeContent(kaze.$content, response);
+	});
+
+	kaze.markActiveLinks(url);
+};
+
+kaze.markActiveLinks = function(url) {
+	if(url === undefined)
+		url = window.location.pathname;
+
+	$('a').each(function() {
+		var $this = $(this);
+		var href = $this.attr('href');
+
+		if(href === url) {
+			$this.addClass('active');
+		} else {
+			$this.removeClass('active');
+		}
+	});
+};
+
+// Run
 kaze.ajaxifyLinks();
+
+// Load history (used on backward and forward button)
+window.addEventListener('popstate', function(e) {
+	var url = e.state;
+	console.log('popstate');
+
+	if(url) {
+		console.log('Load history: ' + url);
+		kaze.loadURL(url, false);
+	} else if(kaze.currentURL !== kaze.originalURL) {
+		console.log('Load non-history: ' + kaze.originalURL);
+		kaze.loadURL(kaze.originalURL, false);
+	}
+});
