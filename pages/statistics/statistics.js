adbed631acf1d713a584234fe835b30adea5906e
Eduard Urbach <e.urbach@gmail.com>
2016-01-02 02:28:59 +0900

    Minor change for statistics

--- pages/statistics/statistics.js
+++ pages/statistics/statistics.js
@@ -6,12 +6,15 @@ exports.get = function(request, response) {
 	let recordCount = 0
 	let gender = {
 		male: 0,
-		female: 0
+		female: 0,
+		unknown: 0
 	}
 
 	arn.scan('Users', function(user) {
 		if(user.gender === 'male' || user.gender === 'female')
 			gender[user.gender] += 1
+		else
+			gender.unknown += 1
 
 		recordCount++
 	}, function() {
