8a9f624e7e10af42da016837d2b6842e8334bc97
Eduard Urbach <e.urbach@gmail.com>
2016-01-02 22:18:32 +0900

    Don't show users without nicknames

--- pages/users/users.js
+++ pages/users/users.js
@@ -7,6 +7,12 @@ exports.get = function(request, response) {
 	let users = []
 
 	arn.scan('Users', function(user) {
+		if(user.nick.startsWith('g') && !isNaN(parseInt(user.nick.substring(1))))
+			return
+
+		if(user.nick.startsWith('fb') && !isNaN(parseInt(user.nick.substring(2))))
+			return
+		
 		user.gravatarURL = gravatar.url(user.email, {s: '50', r: 'x', d: 'mm'}, true)
 		users.push(user)
 	}, function() {
