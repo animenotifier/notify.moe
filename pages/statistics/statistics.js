d041bbab07c7bb9308dda34a43d484395f73bde7
Eduard Urbach <e.urbach@gmail.com>
2016-01-02 10:14:26 +0900

    Updated stats

--- pages/statistics/statistics.js
+++ pages/statistics/statistics.js
@@ -1,6 +1,12 @@
 'use strict'
 
 let arn = require('../../lib')
+let increment = function(obj, key) {
+	if(obj.hasOwnProperty(key))
+		obj[key] += 1
+	else
+		obj[key] = 1
+}
 
 exports.get = function(request, response) {
 	let recordCount = 0
@@ -9,12 +15,21 @@ exports.get = function(request, response) {
 		female: 0,
 		unknown: 0
 	}
+	let providers = {
+		list: {},
+		anime: {},
+		airingDate: {}
+	}
 
 	arn.scan('Users', function(user) {
 		if(user.gender === 'male' || user.gender === 'female')
 			gender[user.gender] += 1
 		else
 			gender.unknown += 1
+
+		increment(providers.list, user.providers.list)
+		increment(providers.anime, user.providers.anime)
+		increment(providers.airingDate, user.providers.airingDate)
 
 		recordCount++
 	}, function() {
@@ -23,6 +38,7 @@ exports.get = function(request, response) {
 				total: recordCount,
 				gender
 			},
+			providers
 		})
 	})
 }
