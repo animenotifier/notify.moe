ae7e1c9779f1a9094e8f590813710228179728ca
Eduard Urbach <e.urbach@gmail.com>
2016-01-03 17:16:23 +0900

    News from Anime News Network on dashboard

---
+++ modules/ann.js
@@ -0,0 +1,37 @@
+'use strict'
+
+let request = require('request-promise')
+let xml2js = require('xml2js')
+let Promise = require('bluebird')
+let arn = require('../lib')
+
+module.exports = function(aero) {
+	let xmlParser = new xml2js.Parser({
+		explicitArray: false,	// Don't put single nodes into an array
+		ignoreAttrs: true,		// Ignore attributes and only create text nodes
+		trim: true,
+		normalize: true,
+		explicitRoot: false
+	})
+
+	Promise.promisifyAll(xmlParser)
+
+	let updateNews = function() {
+		let url = 'http://www.animenewsnetwork.com/news/rss.xml'
+
+		request(url)
+		.then(rss => {
+			return xmlParser.parseStringAsync(rss).then(json => {
+				let items = json.channel.item
+				arn.animeNews = items.filter(item => item.category === 'Anime')
+				arn.gamesNews = items.filter(item => item.category === 'Games')
+			})
+		})
+		.catch(error => {
+			console.log(error)
+		})
+	}
+
+	setInterval(updateNews, 15 * 60 * 1000)
+	updateNews()
+}
--- pages/dashboard/dashboard.js
+++ pages/dashboard/dashboard.js
@@ -1,9 +1,15 @@
 'use strict'
 
+let arn = require('../../lib')
+
 exports.get = function(request, response) {
 	let user = request.user
 
 	response.render({
-		user
+		user,
+		widgets: {
+			'Anime': arn.animeNews,
+			'Games': arn.gamesNews
+		}
 	})
 }
