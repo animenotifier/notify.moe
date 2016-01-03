9fe8c0cbf69415c7b8063b6d65ebca01ddf078a7
Eduard Urbach <e.urbach@gmail.com>
2016-01-03 09:27:58 +0900

    Geolocation

--- modules/serialize.js
+++ modules/serialize.js
@@ -2,6 +2,8 @@
 
 let arn = require('../lib')
 let passport = require('passport')
+let apiKeys = require('../security/api-keys.json')
+let get = require('request-promise')
 
 // Serialize
 // This means we're reducing the user data to a single hash by which the user can be identified.
@@ -11,8 +13,16 @@ passport.serializeUser(function(request, user, done) {
 	user.ip = request.headers['x-forwarded-for'] || request.connection.remoteAddress
 	user.agent = request.headers['user-agent']
 
-	// Save in database
-	arn.setUser(user.id, user)
+	let locationAPI = `http://api.ipinfodb.com/v3/ip-city/?key=${apiKeys.ipInfoDB.clientID}&ip=${user.ip}&format=json`
+
+	get(locationAPI).then(body => {
+		user.location = JSON.parse(body)
+	}).catch(error => {
+		user.location = null
+	}).finally(() => {
+		// Save in database
+		arn.setUser(user.id, user)
+	})
 
 	done(null, user.id)
 })
