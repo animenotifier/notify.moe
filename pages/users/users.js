0b730dca54719acf45baa491651b6e517647af6d
Eduard Urbach <e.urbach@gmail.com>
2016-01-03 10:12:07 +0900

    Additional grouping options for users overview

--- pages/users/users.js
+++ pages/users/users.js
@@ -4,7 +4,48 @@ let arn = require('../../lib')
 let gravatar = require('gravatar')
 
 exports.get = function(request, response) {
-	let users = []
+	let orderBy = request.params[0]
+	let categories = {}
+	let addUser = null
+
+	switch(orderBy) {
+		case 'countries':
+			addUser = user => {
+				let country = user.location.countryName
+
+				if(!country || country === '-')
+					return
+
+				if(categories.hasOwnProperty(country))
+					categories[country].push(user)
+				else
+					categories[country] = [user]
+			}
+			break
+
+		case 'listproviders':
+			addUser = user => {
+				let listProviderName = user.providers.list
+
+				if(!listProviderName)
+					return
+
+				let listProvider = user.listProviders[listProviderName]
+
+				if(!listProvider || !listProvider.userName)
+					return
+
+				if(categories.hasOwnProperty(listProviderName))
+					categories[listProviderName].push(user)
+				else
+					categories[listProviderName] = [user]
+			}
+			break
+
+		default:
+			categories.Users = []
+			addUser = user => categories.Users.push(user)
+	}
 
 	arn.scan('Users', function(user) {
 		if(user.nick.startsWith('g') && !isNaN(parseInt(user.nick.substring(1))))
@@ -12,15 +53,19 @@ exports.get = function(request, response) {
 
 		if(user.nick.startsWith('fb') && !isNaN(parseInt(user.nick.substring(2))))
 			return
-		
+
 		user.gravatarURL = gravatar.url(user.email, {s: '50', r: 'x', d: 'mm'}, true)
-		users.push(user)
+
+		addUser(user)
 	}, function() {
 		// Sort by registration date
-		users.sort((a, b) => new Date(a.registered) - new Date(b.registered))
+		Object.keys(categories).forEach(categoryName => {
+			let category = categories[categoryName]
+			category.sort((a, b) => new Date(a.registered) - new Date(b.registered))
+		})
 
 		response.render({
-			users
+			categories
 		})
 	})
 }
