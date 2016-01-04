5cd89559de80272961752fa24ae08b2a138ea65e
Eduard Urbach <e.urbach@gmail.com>
2016-01-04 10:20:42 +0900

    Display ARN 2.0 donators in user profile

--- modules/login.js
+++ modules/login.js
@@ -3,13 +3,14 @@
 let passport = require('passport')
 let session = require('express-session')
 let apiKeys = require('../security/api-keys.json')
+//let FileStore = require('session-file-store')(session)
 
 module.exports = function(aero) {
 	// Session
 	let sessionOptions = {
 	    name: 'sid',
 	    secret: require('crypto').randomBytes(64).toString('hex'),
-	    saveUninitialized: true,
+	    saveUninitialized: false,
 	    resave: false,
 	    cookie: {
 	        secure: true
