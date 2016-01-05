d9d1172d9a4ba9b539790ddebd92d7dfba5276f0
Eduard Urbach <e.urbach@gmail.com>
2016-01-05 17:43:48 +0900

    Unsubscribe notifications

--- scripts/push-notifications.js
+++ scripts/push-notifications.js
@@ -43,6 +43,50 @@ function subscribe() {
 	});
 }
 
+function unsubscribe() {
+	var pushButton = document.querySelector('.push-button');
+	pushButton.disabled = true;
+
+	navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+		// To unsubscribe from push messaging, you need get the
+		// subscription object, which you can call unsubscribe() on.
+		serviceWorkerRegistration.pushManager.getSubscription().then(function(pushSubscription) {
+			// Check we have a subscription to unsubscribe
+			if(!pushSubscription) {
+				// No subscription object, so set the state
+				// to allow the user to subscribe to push
+				isPushEnabled = false;
+				pushButton.disabled = false;
+				pushButton.textContent = 'Enable Notifications';
+				return;
+			}
+
+			var subscriptionId = pushSubscription.subscriptionId;
+			// TODO: Make a request to your server to remove
+			// the subscriptionId from your data store so you
+			// don't attempt to send them push messages anymore
+
+			// We have a subscription, so call unsubscribe on it
+			pushSubscription.unsubscribe().then(function(successful) {
+				pushButton.disabled = false;
+				pushButton.textContent = 'Enable Notifications';
+				isPushEnabled = false;
+			}).catch(function(e) {
+				// We failed to unsubscribe, this can lead to
+				// an unusual state, so may be best to remove
+				// the users data from your data store and
+				// inform the user that you have done so
+
+				console.log('Unsubscription error: ', e);
+				pushButton.disabled = false;
+				pushButton.textContent = 'Enable Notifications';
+			});
+		}).catch(function(e) {
+			console.error('Error thrown while unsubscribing from push messaging.', e);
+		});
+	});
+}
+
 // Once the service worker is registered set the initial state
 function initialiseState(registration) {
 	console.log('Initialise state...');
