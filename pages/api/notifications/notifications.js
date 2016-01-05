f8a71e6d48d9aea2b12bec8593044cc287e5e121
Eduard Urbach <e.urbach@gmail.com>
2016-01-05 21:39:59 +0900

    Further testing dummy notifications

--- pages/api/notifications/notifications.js
+++ pages/api/notifications/notifications.js
@@ -1,9 +1,11 @@
 'use strict'
 
 exports.get = (request, response) => {
+	let user = request.user
+
 	response.json({
 		notification: {
-			title: 'Yay a message.',
+			title: user ? (user.nick + ', there is a new episode') : 'Yay a message.',
 			message: 'We have received a push message.',
 			icon: '/images/characters/arn-waifu.png',
 			tag: 'demo'
