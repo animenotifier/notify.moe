7adb50c48ce09cda042170e8fc5dda9b87867c86
Eduard Urbach <e.urbach@gmail.com>
2016-01-05 21:35:05 +0900

    Trying dummy notifications

--- pages/api/notifications/notifications.js
+++ pages/api/notifications/notifications.js
@@ -2,6 +2,11 @@
 
 exports.get = (request, response) => {
 	response.json({
-		
+		notification: {
+			title: 'Yay a message.',
+			message: 'We have received a push message.',
+			icon: '/images/characters/arn-waifu.png',
+			tag: 'demo'
+		}
 	})
 }
