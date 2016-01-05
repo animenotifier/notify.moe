416eb6693a56f10efa945f426335fd1b4cbdb067
Eduard Urbach <e.urbach@gmail.com>
2016-01-05 17:39:38 +0900

    Updated to latest standards for push subscriptions

--- scripts/push-notifications.js
+++ scripts/push-notifications.js
@@ -12,33 +12,34 @@ function subscribe() {
 	pushButton.disabled = true;
 
 	navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-		serviceWorkerRegistration.pushManager.subscribe()
-			.then(function(subscription) {
-				// The subscription was successful
-				isPushEnabled = true;
-				pushButton.textContent = 'Disable Notifications';
-				pushButton.disabled = false;
+		serviceWorkerRegistration.pushManager.subscribe({
+			userVisibleOnly: true
+		}).then(function(subscription) {
+			// The subscription was successful
+			isPushEnabled = true;
+			pushButton.textContent = 'Disable Notifications';
+			pushButton.disabled = false;
 
-				// TODO: Send the subscription.endpoint to your server
-				// and save it to send a push message at a later date
-				return sendSubscriptionToServer(subscription);
-			}).catch(function(e) {
-				if(Notification.permission === 'denied') {
-					// The user denied the notification permission which
-					// means we failed to subscribe and the user will need
-					// to manually change the notification permission to
-					// subscribe to push messages
-					console.warn('Permission for Notifications was denied');
-					pushButton.disabled = true;
-				} else {
-					// A problem occurred with the subscription; common reasons
-					// include network errors, and lacking gcm_sender_id and/or
-					// gcm_user_visible_only in the manifest.
-					console.error('Unable to subscribe to push.', e);
-					pushButton.disabled = false;
-					pushButton.textContent = 'Enable Notifications';
-				}
-			});
+			// TODO: Send the subscription.endpoint to your server
+			// and save it to send a push message at a later date
+			return sendSubscriptionToServer(subscription);
+		}).catch(function(e) {
+			if(Notification.permission === 'denied') {
+				// The user denied the notification permission which
+				// means we failed to subscribe and the user will need
+				// to manually change the notification permission to
+				// subscribe to push messages
+				console.warn('Permission for Notifications was denied');
+				pushButton.disabled = true;
+			} else {
+				// A problem occurred with the subscription; common reasons
+				// include network errors, and lacking gcm_sender_id and/or
+				// gcm_user_visible_only in the manifest.
+				console.error('Unable to subscribe to push.', e);
+				pushButton.disabled = false;
+				pushButton.textContent = 'Enable Notifications';
+			}
+		});
 	});
 }
 
