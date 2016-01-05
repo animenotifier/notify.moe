f8efa5bd6c8194d2483bb72581c8c5b6130a869f
Eduard Urbach <e.urbach@gmail.com>
2016-01-05 20:13:55 +0900

    Updated service worker

--- service-worker.js
+++ service-worker.js
@@ -4,23 +4,6 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('push', function(event) {
-	console.log('Received a push message', event);
-
-	var title = 'Yay a message.';
-	var body = 'We have received a push message.';
-	var icon = '/images/characters/arn-waifu.png';
-	var tag = 'simple-push-demo-notification-tag';
-
-	event.waitUntil(
-		self.registration.showNotification(title, {
-			body: body,
-			icon: icon,
-			tag: tag
-		})
-	);
-});
-
-self.addEventListener('push', function(event) {
 	// Since there is no payload data with the first version
 	// of push messages, we'll grab some data from
 	// an API and use it to populate a notification
