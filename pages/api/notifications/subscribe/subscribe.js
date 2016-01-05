7f53d0a856fafdc0bb167ac6da18f427dde1dc74
Eduard Urbach <e.urbach@gmail.com>
2016-01-05 23:05:01 +0900

    Restored old version

--- pages/api/notifications/subscribe/subscribe.js
+++ pages/api/notifications/subscribe/subscribe.js
@@ -4,7 +4,6 @@ exports.post = (request, response) => {
 	let endpoint = null
 
 	console.log(request.body)
-	console.log(JSON.parse(request.body))
 
 	try {
 		endpoint = JSON.parse(request.body).endpoint
