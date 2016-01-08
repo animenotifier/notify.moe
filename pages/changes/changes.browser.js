86cf66622cdb7ebb187d2898515e10998f7eb729
Eduard Urbach <e.urbach@gmail.com>
2016-01-09 02:01:31 +0900

    Fixed time view in changelog

--- pages/changes/changes.browser.js
+++ pages/changes/changes.browser.js
@@ -2,24 +2,24 @@ var timeSince = function(start, date) {
 	var seconds = Math.floor((start - date) / 1000);
 
 	var interval = Math.floor(seconds / 31536000);
-	if(interval > 1)
-	    return interval + ' years';
+	if(interval >= 1)
+	    return interval + ' year' + (interval !== 1 ? 's' : '');
 
 	interval = Math.floor(seconds / 2592000);
-	if(interval > 1)
-	    return interval + ' months';
+	if(interval >= 1)
+	    return interval + ' month' + (interval !== 1 ? 's' : '');
 
 	interval = Math.floor(seconds / 86400);
-	if(interval > 1)
-	    return interval + ' days';
+	if(interval >= 1)
+	    return interval + ' day' + (interval !== 1 ? 's' : '');
 
 	interval = Math.floor(seconds / 3600);
-	if(interval > 1)
-	    return interval + ' hours';
+	if(interval >= 1)
+	    return interval + ' hour' + (interval !== 1 ? 's' : '');
 
 	interval = Math.floor(seconds / 60);
-	if(interval > 1)
-	    return interval + ' minutes';
+	if(interval >= 1)
+	    return interval + ' minute' + (interval !== 1 ? 's' : '');
 
 	return Math.floor(seconds) + ' seconds';
 };
