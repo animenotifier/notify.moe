a395ea58b0fc12a89b4b5540d04c36ff2667709f
Eduard Urbach <e.urbach@gmail.com>
2016-01-10 00:16:29 +0900

    Improved TCP settings

--- firewall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-sugoi clearFirewall
-sugoi redirectPort 80 5000
-sugoi redirectPort 443 5001
-sugoi blockPort 3000
---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Firewall
+sugoi clearFirewall
+sugoi redirectPort 80 5000
+sugoi redirectPort 443 5001
+sugoi blockPort 3000
+
+# Disable swap
+sudo sysctl vm.swappiness=0
+
+# TCP settings
+sudo sysctl net.ipv4.ip_local_port_range="15000 61000"
+sudo sysctl net.ipv4.tcp_fin_timeout=15
+sudo sysctl net.ipv4.tcp_tw_recycle=1
+sudo sysctl net.ipv4.tcp_tw_reuse=1
+sudo sysctl net.ipv4.tcp_max_syn_backlog=40000
+sudo sysctl net.ipv4.tcp_sack=1
+sudo sysctl net.ipv4.tcp_window_scaling=1
+sudo sysctl net.ipv4.tcp_keepalive_intvl=30
+sudo sysctl net.ipv4.tcp_moderate_rcvbuf=1
+
+# net.core
+sudo sysctl net.core.somaxconn=1024
+#sudo sysctl net.core.wmem_default=8388608
+#sudo sysctl net.core.rmem_default=8388608
+
+# Max files
+ulimit -n 2048
