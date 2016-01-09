981bc874eb3cab373adc8cbc30cb17a53e26d565
Eduard Urbach <e.urbach@gmail.com>
2016-01-09 15:28:41 +0900

    Shortened 'episodes behind' to '+'

--- pages/user/user.browser.js
+++ pages/user/user.browser.js
@@ -47,62 +47,56 @@ window.loadAnimeList = function() {
 
 			item.appendChild(link);
 
+			var addIconLink = function(iconName, url, linkClass, tooltip) {
+				var link = document.createElement('a');
+				link.href = url;
+				link.target = '_blank';
+				link.className = linkClass;
+				link.title = tooltip;
+
+				var icon = document.createElement('div');
+				icon.className = 'glyphicon glyphicon-' + iconName;
+				link.appendChild(icon);
+
+				item.appendChild(link);
+			}
+
 			if(loggedIn) {
 				if(anime.episodes.watched > 0 && anime.episodes.watched === anime.episodes.max) {
-					var completed = document.createElement('a');
-					completed.href = response.listUrl;
-					completed.target = '_blank';
-					completed.className = 'anime-completed';
-					completed.title = 'You completed this anime.';
-
-					var icon = document.createElement('div');
-					icon.className = 'glyphicon glyphicon-ok-sign';
-					completed.appendChild(icon);
-
-					item.appendChild(completed);
+					addIconLink(
+						'ok-sign',
+						response.listUrl,
+						'anime-completed',
+						'You completed this anime.'
+					);
 				} else if(anime.episodes.available >= anime.episodes.next) {
-					var download = document.createElement('a');
-					download.href = anime.animeProvider.nextEpisodeUrl;
-					download.target = '_blank';
-					download.className = 'anime-download-link';
-
-					if(anime.episodes.next && anime.episodes.next !== 0)
-						download.title = 'Download episode ' + anime.episodes.next;
-
-					var icon = document.createElement('div');
-					icon.className = 'glyphicon glyphicon-cloud-download';
-					download.appendChild(icon);
-					item.appendChild(download);
+					addIconLink(
+						'cloud-download',
+						anime.animeProvider.nextEpisodeUrl,
+						'anime-download-link',
+						'Download episode ' + anime.episodes.next
+					);
 
 					var behind = (anime.episodes.available - anime.episodes.watched);
 					var episodes = document.createElement('span');
 					episodes.className = 'episodes-behind';
-					episodes.appendChild(document.createTextNode(behind + (behind === 1 ? ' episode' : ' episodes') + ' behind'));
+					episodes.appendChild(document.createTextNode('+' + behind));
+					episodes.title = behind + (behind === 1 ? ' episode' : ' episodes') + ' behind';
 					item.appendChild(episodes);
 				} else if(anime.episodes.available > 0 && anime.episodes.available === anime.episodes.watched) {
-					var ok = document.createElement('a');
-					ok.href = anime.animeProvider.nextEpisodeUrl;
-					ok.target = '_blank';
-					ok.className = 'anime-up-to-date';
-					ok.title = 'You watched ' + anime.episodes.watched + ' out of ' + anime.episodes.available + ' available.';
-
-					var icon = document.createElement('div');
-					icon.className = 'glyphicon glyphicon-ok';
-					ok.appendChild(icon);
-
-					item.appendChild(ok);
+					addIconLink(
+						'ok',
+						anime.animeProvider.nextEpisodeUrl,
+						'anime-up-to-date',
+						'You watched ' + anime.episodes.watched + ' out of ' + anime.episodes.available + ' available.'
+					);
 				} else if(anime.episodes.available === 0) {
-					var warning = document.createElement('a');
-					warning.href = anime.animeProvider.url;
-					warning.target = '_blank';
-					warning.className = 'anime-warning';
-					warning.title = 'Could not find your anime title on the anime provider.';
-
-					var icon = document.createElement('div');
-					icon.className = 'glyphicon glyphicon-exclamation-sign';
-					warning.appendChild(icon);
-
-					item.appendChild(warning);
+					addIconLink(
+						'exclamation-sign',
+						anime.animeProvider.url,
+						'anime-warning',
+						'Could not find your anime title on the anime provider.'
+					);
 				}
 			}
 
