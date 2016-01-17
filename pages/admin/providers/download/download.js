1720d5d065dccae874e8147de7fff97adec221b1
Eduard Urbach <e.urbach@gmail.com>
2016-01-17 22:53:54 +0900

    Added errors from anime providers to admin panel

--- pages/admin/providers/download/download.js
+++ pages/admin/providers/download/download.js
@@ -1,9 +1,6 @@
 'use strict'
 
-
 let Promise = require('bluebird')
-
-const listLength = 15
 
 exports.get = (request, response) => {
 	if(!arn.auth(request, response, 'editor'))
@@ -12,9 +9,7 @@ exports.get = (request, response) => {
 	let user = request.user
 
 	let providers = [
-		'MyAnimeList',
-		'HummingBird',
-		'AnimePlanet'
+		'Nyaa'
 	]
 
 	let matches = providers.reduce((obj, provider) => {
@@ -22,24 +17,19 @@ exports.get = (request, response) => {
 		return obj
 	}, {})
 
-	let tasks = providers.map(provider => arn.filter('Match' + provider, record => !record.edited && record.similarity !== 1).then(uneditedMatches => matches[provider] = uneditedMatches))
+	let tasks = providers.map(provider => arn.filter('AnimeTo' + provider, record => !record.edited && record.episodes === 0).then(uneditedMatches => matches[provider] = uneditedMatches))
 
 	Promise.all(tasks).then(() => {
-		providers.forEach(provider => {
+		/*providers.forEach(provider => {
 			matches[provider].sort((a, b) => a.similarity > b.similarity ? 1 : -1)
 
 			if(matches[provider].length >= listLength)
 				matches[provider].length = listLength
-		})
+		})*/
 
 		response.render({
 			user,
-			matches,
-			linkPrefixes: {
-				MyAnimeList: 'http://myanimelist.net/anime/',
-				HummingBird: 'https://hummingbird.me/anime/',
-				AnimePlanet: 'http://anime-planet.com/anime/'
-			}
+			matches
 		})
 	})
 }
---
+++ pages/admin/providers/list/list.js
@@ -0,0 +1,44 @@
+'use strict'
+
+let Promise = require('bluebird')
+
+const listLength = 15
+
+exports.get = (request, response) => {
+	if(!arn.auth(request, response, 'editor'))
+		return
+
+	let user = request.user
+
+	let providers = [
+		'MyAnimeList',
+		'HummingBird',
+		'AnimePlanet'
+	]
+
+	let matches = providers.reduce((obj, provider) => {
+		obj[provider] = []
+		return obj
+	}, {})
+
+	let tasks = providers.map(provider => arn.filter('Match' + provider, record => !record.edited && record.similarity !== 1).then(uneditedMatches => matches[provider] = uneditedMatches))
+
+	Promise.all(tasks).then(() => {
+		providers.forEach(provider => {
+			matches[provider].sort((a, b) => a.similarity > b.similarity ? 1 : -1)
+
+			if(matches[provider].length >= listLength)
+				matches[provider].length = listLength
+		})
+
+		response.render({
+			user,
+			matches,
+			linkPrefixes: {
+				MyAnimeList: 'http://myanimelist.net/anime/',
+				HummingBird: 'https://hummingbird.me/anime/',
+				AnimePlanet: 'http://anime-planet.com/anime/'
+			}
+		})
+	})
+}
