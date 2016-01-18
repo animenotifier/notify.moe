e147d9745091b2bb31634b61f5f58d6993eeb6ac
Eduard Urbach <e.urbach@gmail.com>
2016-01-18 22:30:26 +0900

    Added more anime details

---
+++ patches/anilist-details.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let aero = require('aero')
+let arn = require('../lib')
+let request = require('request-promise')
+let Promise = require('bluebird')
+let database = require('../modules/database')
+let RateLimiter = require('limiter').RateLimiter
+let limiter = new RateLimiter(1, 500)
+
+database(aero, Promise.coroutine(function*(error) {
+	yield arn.listProviders.AniList.authorize()
+
+	arn.forEach('Anime', anime => {
+		// Skip anime that have been imported already
+		if(anime.description)
+			return
+
+		limiter.removeTokens(1, function() {
+			arn.listProviders.AniList.getAnimeDetails(anime.id).then(details => {
+				arn.set('Anime', anime.id, details).then(() => console.log(`Finished importing anime ${anime.id}`))
+			})
+		})
+	})
+}))
