27f84f472aa998946290ded5d668e41ef7b451e1
Eduard Urbach <e.urbach@gmail.com>
2016-01-19 19:01:02 +0900

    Genre cache

--- pages/anime/genres/genres.js
+++ pages/anime/genres/genres.js
@@ -1,17 +1,45 @@
 'use strict'
 
-exports.get = function(request, response) {
+let fs = require('fs')
+let AutoUpdateCache = require('../../../lib/classes/AutoUpdateCache')
+
+let genres = new AutoUpdateCache(() => {
+	console.log('Updating genre cache...')
+
+	let allGenres = {}
+	let genreList = fs.readFileSync('pages/anime/genres/genres.txt', 'utf8').split('\n')
+	let tasks = []
+
+	genreList.forEach(genre => {
+		console.log('Updating cache for genre:', genre)
+
+		genre = genre.toLowerCase()
+		let genreSearch = `;${genre};`
+
+		tasks.push(arn.filter('Anime', anime => anime.genres && (';' + anime.genres.join(';').toLowerCase() + ';').indexOf(genreSearch) !== -1).then(animeList => {
+			animeList.sort((a, b) => {
+				if(!a.startDate)
+					return 1
+
+				if(!b.startDate)
+					return -1
+
+				return a.startDate > b.startDate ? -1 : 1
+			})
+
+			allGenres[genre] = animeList
+		}))
+	})
+
+	return Promise.all(tasks).then(() => allGenres)
+}, 30 * 60, {})
+
+exports.get = (request, response) => {
 	let genre = request.params[0]
-	let genreSearch = `;${genre};`
+	let animeList = genres.cache[genre]
 
-	arn.filter('Anime', anime => anime.genres && (';' + anime.genres.join(';').toLowerCase() + ';').indexOf(genreSearch) !== -1).then(animeList => {
-		animeList.sort((a, b) => {
-			return a.startDate > b.startDate ? -1 : 1
-		})
-
-		response.render({
-			genre,
-			animeList
-		})
+	response.render({
+		genre,
+		animeList
 	})
 }
