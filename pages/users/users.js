c937a841fc655d1197b1c0d0191008fe57e4a090
Eduard Urbach <e.urbach@gmail.com>
2016-01-20 00:00:15 +0900

    Use arn.repeatedly for user cache

--- pages/users/users.js
+++ pages/users/users.js
@@ -2,94 +2,123 @@
 
 let gravatar = require('gravatar')
 
-exports.get = function(request, response) {
-	let orderBy = request.params[0]
-	let addUser = null
-	let now = new Date()
-	let cacheKey = `users:${orderBy}`
+let getAddUserFunction = (orderBy) => {
+	return (user, categories) => {
+		let date = new Date(orderBy === 'joindate' ? user.registered : user.lastLogin)
+		let now = new Date()
+		let seconds = Math.floor((now - date) / 1000)
+		let days = seconds / 60 / 60 / 24
+		let categoryName = 'Ojii-san'
 
-	arn.userListCache.get(cacheKey, function(error, categories) {
-		if(!error && categories) {
-			response.render({
-				categories
-			})
-			return
+		if(days <= 1)
+			categoryName = 'Last 24 hours'
+		else if(days <= 2)
+			categoryName = 'Yesterday'
+		else if(days <= 7)
+			categoryName = 'This week'
+		else if(days <= 30)
+			categoryName = 'This month'
+
+		if(categories.hasOwnProperty(categoryName))
+			categories[categoryName].push(user)
+		else
+			categories[categoryName] = [user]
+
+		return true
+	}
+}
+
+let orderByMethods = {
+	countries: {
+		getCategories: () => {
+			return {}
+		},
+
+		addUser: (user, categories) => {
+			if(!user.location)
+				return false
+
+			let country = user.location.countryName
+
+			if(!country || country === '-')
+				return false
+
+			if(categories.hasOwnProperty(country))
+				categories[country].push(user)
+			else
+				categories[country] = [user]
+
+			return true
 		}
+	},
 
-		categories = {}
+	listproviders: {
+		getCategories: () => {
+			return {
+				'AniList': [],
+				'MyAnimeList': [],
+				'HummingBird': [],
+				'AnimePlanet': []
+			}
+		},
 
-		switch(orderBy) {
-			case 'countries':
-				addUser = user => {
-					if(!user.location)
-						return false
+		addUser: (user, categories) => {
+			if(categories.hasOwnProperty(user.providers.list))
+				categories[user.providers.list].push(user)
+			else
+				categories[user.providers.list] = [user]
 
-					let country = user.location.countryName
-
-					if(!country || country === '-')
-						return false
-
-					if(categories.hasOwnProperty(country))
-						categories[country].push(user)
-					else
-						categories[country] = [user]
-
-					return true
-				}
-				break
-
-			case 'listproviders':
-				addUser = user => {
-					if(categories.hasOwnProperty(user.providers.list))
-						categories[user.providers.list].push(user)
-					else
-						categories[user.providers.list] = [user]
-
-					return true
-				}
-				break
-
-			case 'login':
-			case 'registration':
-				// Force a special order
-				categories = {
-					'Last 24 hours': [],
-					'Yesterday': [],
-					'This week': [],
-					'This month': [],
-					'Ojii-san': []
-				}
-
-				addUser = user => {
-					let date = new Date(orderBy === 'registration' ? user.registered : user.lastLogin)
-					let seconds = Math.floor((now - date) / 1000)
-					let days = seconds / 60 / 60 / 24
-					let categoryName = 'Ojii-san'
-
-					if(days <= 1)
-						categoryName = 'Last 24 hours'
-					else if(days <= 2)
-						categoryName = 'Yesterday'
-					else if(days <= 7)
-						categoryName = 'This week'
-					else if(days <= 30)
-						categoryName = 'This month'
-
-					if(categories.hasOwnProperty(categoryName))
-						categories[categoryName].push(user)
-					else
-						categories[categoryName] = [user]
-
-					return true
-				}
-				break
-
-			default:
-				categories.All = []
-				addUser = user => categories.All.push(user)
+			return true
 		}
+	},
 
-		arn.filter('Users', user => arn.isActiveUser(user) && addUser(user)).then(users => {
+	login: {
+		getCategories: () => {
+			return {
+				'Last 24 hours': [],
+				'Yesterday': [],
+				'This week': [],
+				'This month': [],
+				'Ojii-san': []
+			}
+		},
+
+		addUser: getAddUserFunction('login')
+	},
+
+	joindate: {
+		getCategories: () => {
+			return {
+				'Last 24 hours': [],
+				'Yesterday': [],
+				'This week': [],
+				'This month': [],
+				'Ojii-san': []
+			}
+		},
+
+		addUser: getAddUserFunction('joindate')
+	},
+
+	default: {
+		getCategories: () => {
+			return {
+				All: []
+			}
+		},
+
+		addUser: (user, categories) => categories.All.push(user)
+	}
+}
+
+arn.repeatedly(5 * 60, () => {
+	Object.keys(orderByMethods).forEach(orderBy => {
+		let method = orderByMethods[orderBy]
+		let categories = method.getCategories()
+		let addUser = method.addUser
+		let cacheKey = `users:${orderBy}`
+
+		arn.filter('Users', user => arn.isActiveUser(user) && addUser(user, categories)).then(users => {
 			users.forEach(user => {
 				user.gravatarURL = gravatar.url(user.email, {s: '50', r: 'x', d: '404'}, true)
 			})
@@ -100,11 +129,30 @@ exports.get = function(request, response) {
 				category.sort((a, b) => new Date(a.registered) - new Date(b.registered))
 			})
 
-			response.render({
+			arn.set('Cache', cacheKey, {
 				categories
 			})
-
-			arn.userListCache.set(cacheKey, categories, () => {})
 		})
+	})
+})
+
+exports.get = function(request, response) {
+	let orderBy = request.params[0] || 'default'
+	let cacheKey = `users:${orderBy}`
+
+	arn.get('Cache', cacheKey).then(record => {
+		// We need to copy the object to keep the order.
+		// It sucks but the database doesn't keep object properties' order.
+		let categories = orderByMethods[orderBy].getCategories()
+
+		Object.keys(record.categories).forEach(categoryName => {
+			categories[categoryName] = record.categories[categoryName]
+		})
+
+		response.render({
+			categories
+		})
+	}).catch(error => {
+		response.render({})
 	})
 }
