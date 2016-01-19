af4ebf6207f3f0954389f5643d5ae7fc8421960b
Eduard Urbach <e.urbach@gmail.com>
2016-01-19 22:37:03 +0900

    Genre lists are now cached in DB

--- pages/anime/genres/genres.js
+++ pages/anime/genres/genres.js
@@ -4,8 +4,6 @@ let Promise = require('bluebird')
 let repeatedly = require('../../../lib/utils/repeatedly')
 let fs = Promise.promisifyAll(require('fs'))
 
-let genres = {}
-
 repeatedly(30 * 60, () => {
 	console.log('Updating genre cache...')
 
@@ -14,17 +12,16 @@ repeatedly(30 * 60, () => {
 		let tasks = []
 
 		genreList.forEach(genre => {
-			console.log(genre)
-
 			genre = arn.fixGenre(genre)
 			let genreSearch = `;${genre};`
 
-			tasks.push(Promise.delay(tasks.length * 500).then(() => {
-				console.log('Updating genre:', genre)
-
+			tasks.push(Promise.delay(tasks.length * 1000).then(() => {
 				let animeList = []
 
 				return arn.forEach('Anime', anime => {
+					if(!anime.watching)
+						return
+
 					if(!anime.genres)
 						return
 
@@ -39,16 +36,27 @@ repeatedly(30 * 60, () => {
 					})
 				}).then(() => {
 					animeList.sort((a, b) => {
-						if(!a.startDate)
+						if(a.watching === b.watching) {
+							if(!a.startDate)
+								return 1
+
+							if(!b.startDate)
+								return -1
+
+							return a.startDate > b.startDate ? -1 : 1
+						} else if(!a.watching) {
 							return 1
-
-						if(!b.startDate)
+						} else if(!b.watching) {
 							return -1
-
-						return a.startDate > b.startDate ? -1 : 1
+						} else {
+							return a.watching > b.watching ? -1 : 1
+						}
 					})
 
-					genres[genre] = animeList
+					arn.set('Genres', genre, animeList)
+
+					if(global.gc)
+						global.gc()
 				})
 			}))
 		})
@@ -57,10 +65,20 @@ repeatedly(30 * 60, () => {
 
 exports.get = (request, response) => {
 	let genre = request.params[0]
-	let animeList = genres[genre]
 
-	response.render({
-		genre,
-		animeList
+	if(!genre) {
+		response.render({})
+		return
+	}
+
+	arn.get('Genres', genre).then(animeList => {
+		response.render({
+			genre,
+			animeList
+		})
+	}).catch(error => {
+		console.error(error, error.stack)
+		response.render({})
+		return
 	})
 }
