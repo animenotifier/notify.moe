693401284afc3f031a9870aa08d60e6061291e68
Eduard Urbach <e.urbach@gmail.com>
2016-01-31 10:50:26 +0900

    Rewrite /+me to own profile URL

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ aero.rewrite(function(request, response) {
         return true
     }
 
-	if(request.url.startsWith('/+'))
+	if(request.user && request.url.startsWith('/+me'))
+		request.url = '/user/' + request.user.nick + request.url.substring(4)
+	else if(request.url.startsWith('/+'))
 		request.url = '/user/' + request.url.substring(2)
 	else if(request.url.startsWith('/_/+'))
 		request.url = '/_/user/' + request.url.substring(4)
