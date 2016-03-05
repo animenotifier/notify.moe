4ce671ec1a4892dd85c0687810683a57a7eda213
Eduard Urbach <e.urbach@gmail.com>
2016-03-05 16:40:30 +0900

    Minor changes

--- scripts/init.js
+++ scripts/init.js
@@ -1,9 +1,9 @@
 // Fix Facebook login hash in URL
 if(window.location.hash && window.location.hash === '#_=_') {
-	window.history.pushState('', document.title, window.location.pathname);
+	window.history.pushState('', document.title, window.location.pathname)
 }
 
 // Fade out loading animation
 document.addEventListener('DOMContentLoaded', function(event) {
-	$.loadingAnimation.classList.add('fade-out');
-});
+	$.loadingAnimation.classList.add('fade-out')
+})
