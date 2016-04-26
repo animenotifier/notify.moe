b6b72d25e51df1149bf2f680e9f3b2fa2d63f5d9
Eduard Urbach <e.urbach@gmail.com>
2016-04-26 11:04:33 +0900

    Updated anime importer

--- jobs/sync-anime.js
+++ jobs/sync-anime.js
@@ -5,7 +5,7 @@ let importAnimeFromAniList = coroutine(function*() {
 
 	yield arn.listProviders.AniList.authorize()
 
-	let maxPage = 240
+	let maxPage = 250
 	for(let page = maxPage; page >= 1; page--) {
 		yield Promise.delay(1100)
 
