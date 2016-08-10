fe710fa4b96eea7dfdb25e1fe4259dd8753dd33e
Eduard Urbach <e.urbach@gmail.com>
2016-08-11 08:58:07 +0900

    Improved username fix

--- patches/fix-user-names.js
+++ patches/fix-user-names.js
@@ -1,19 +1,18 @@
 'use strict'
 
-let aero = require('aero')
-let database = require('../modules/database')
-let arn = require('../lib')
+var arn = require('../lib')
 
-database(aero, function(error) {
+arn.db.ready.then(() => {
 	let tasks = []
 
     arn.forEach('Users', function(user) {
 		let listProviderName = user.providers.list
         if(user.listProviders[listProviderName] && user.listProviders[listProviderName].userName) {
             let userName = user.listProviders[listProviderName].userName
-
+			
+			let old = user.listProviders[listProviderName].userName
 			user.listProviders[listProviderName].userName = arn.fixListProviderUserName(userName)
-			console.log(user.listProviders[listProviderName].userName)
+			console.log(old, '->', user.listProviders[listProviderName].userName)
 
 			tasks.push(arn.set('Users', user.id, user))
         }
