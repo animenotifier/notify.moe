c9df12004194d0ffe3a88a7fe5cb1cd63584477a
Eduard Urbach <e.urbach@gmail.com>
2016-08-11 09:48:28 +0900

    Show server status on /status

---
+++ jobs/status.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const hosts = [
+	'https://anilist.co',
+	'https://hummingbird.me',
+	'http://myanimelist.net',
+	'http://www.anime-planet.com',
+	'http://www.crunchyroll.com',
+	'https://www.nyaa.se'
+]
+
+let request = require('request-promise')
+
+let updateStatus = coroutine(function*() {
+	let status = []
+	
+	console.log(chalk.yellow('✖'), 'Updating status...')
+	
+	for(let host of hosts) {
+		yield Promise.delay(100)
+		
+		yield request({
+			uri: host,
+			method: 'GET',
+			headers: {
+				'User-Agent': 'Anime Release Notifier'
+			}
+		})
+		.then(body => {
+			console.log(chalk.green('✔'), host, 'alive')
+			
+			status.push({
+				context: host,
+				error: ''
+			})
+		})
+		.catch(e => {
+			console.log(chalk.red('✖'), host, 'dead')
+			
+			status.push({
+				context: host,
+				error: e.toString()
+			})
+		})
+	}
+	
+	arn.set('Cache', 'status', status)
+})
+
+arn.repeatedly(5 * minutes, updateStatus)
---
+++ pages/status/status.js
@@ -0,0 +1,10 @@
+'use strict'
+
+exports.get = function*(request, response) {
+	let status = yield arn.get('Cache', 'status')
+	
+	response.render({
+		user: request.user,
+		status
+	})
+}
