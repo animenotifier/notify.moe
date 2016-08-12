61d78025165a4dec362650f89d8884bfae4232e9
Eduard Urbach <e.urbach@gmail.com>
2016-08-12 11:23:30 +0900

    Implemented message deletion

---
+++ pages/api/messages/delete/delete.js
@@ -0,0 +1,22 @@
+exports.post = function*(request, response) {
+	let user = request.user
+
+	if(!user) {
+		response.writeHead(409)
+		response.end('Not logged in')
+		return
+	}
+	
+	let messageId = request.params[0]
+	
+	if(!messageId) {
+		response.writeHead(409)
+		response.end('No message specified')
+		return
+	}
+	
+	yield arn.remove('Messages', messageId)
+	
+	console.log(`${user.nick} deleted the message '${messageId}'`)
+	response.end('success')
+}
--- pages/user/user.client.js
+++ pages/user/user.client.js
@@ -170,12 +170,13 @@ window.loadAnimeList = function(clearCache) {
 	});
 };
 
-window.editMessage = window.deleteMessage = function() {
+window.editMessage = function() {
 	alert('work in progress')
 }
 
 window.likeMessage = messageId => $.post('/api/messages/like/' + messageId).then(window.loadMessages)
 window.unlikeMessage = messageId => $.post('/api/messages/unlike/' + messageId).then(window.loadMessages)
+window.deleteMessage = messageId => $.post('/api/messages/delete/' + messageId).then(window.loadMessages)
 
 window.sendMessage = function() {
 	let userName = $('nick').textContent
