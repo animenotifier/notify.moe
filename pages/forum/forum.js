d4030a33925a6635400c6421501d2649b502cdf3
Eduard Urbach <e.urbach@gmail.com>
2016-08-13 12:57:44 +0900

    Fixed thread sorting

--- pages/forum/forum.js
+++ pages/forum/forum.js
@@ -1,4 +1,10 @@
 const maxThreadCount = 13
+const tagToIcon = {
+	general: 'paperclip',
+	anime: 'television',
+	suggestion: 'lightbulb-o',
+	bug: 'bug'
+}
 
 exports.get = function*(request, response) {
 	let user = request.user
@@ -10,38 +16,46 @@ exports.get = function*(request, response) {
 	else
 		threads = yield arn.filter('Threads', thread => thread.tags && thread.tags.indexOf(tag) !== -1)
 	
-	const testTexts = [
-		'A Guide to Forum Flags	',
-		'Guidelines	',
-		'Ban the User Above You #3 (with hashtags)	',
-		'Post a Random Fact About the User Above You #8 Electric Boogaloo	',
-		'Should There Be a Season Three of “Chuunibyou demo Koi ga Shitai”?	',
-		'Why would anyone vote for Trump?	',
-		'Count to 10k Thread V2	',
-		'Watched or Not Watched?	',
-		'Guess the Above User’s Gender	',
-		'Shutting Down Hummingbird	',
-		'Anime like Mouryou no Hako? new	',
-		'Any Overwatch PS4 Players? new	',
-		'Any anime inspired by NGE?	'
-	]
-	
-	for(let i = 0; i < 15; i++) {
-		threads.push({
-			title: testTexts[i % testTexts.length],
-			text: testTexts[i % testTexts.length],
-			authorId: ['HyhW-TsW', 'VkBaMJ6ux', 'NyGyZ2xwe', 'EkffWKXte'][i % 4],
-			id: 'test' + i,
-			sticky: 0,
-			created: (new Date()).toISOString()
-		})
-	}
-	
-	threads[13].sticky = true
+	// const testTexts = [
+	// 	'A Guide to Forum Flags	',
+	// 	'Guidelines	',
+	// 	'Ban the User Above You #3 (with hashtags)	',
+	// 	'Post a Random Fact About the User Above You #8 Electric Boogaloo	',
+	// 	'Should There Be a Season Three of “Chuunibyou demo Koi ga Shitai”?	',
+	// 	'Why would anyone vote for Trump?	',
+	// 	'Count to 10k Thread V2	',
+	// 	'Watched or Not Watched?	',
+	// 	'Guess the Above User’s Gender	',
+	// 	'Shutting Down Hummingbird	',
+	// 	'Anime like Mouryou no Hako? new	',
+	// 	'Any Overwatch PS4 Players? new	',
+	// 	'Any anime inspired by NGE?	'
+	// ]
+	// 
+	// for(let i = 0; i < 15; i++) {
+	// 	threads.push({
+	// 		title: testTexts[i % testTexts.length],
+	// 		text: testTexts[i % testTexts.length],
+	// 		authorId: ['HyhW-TsW', 'VkBaMJ6ux', 'NyGyZ2xwe', 'EkffWKXte'][i % 4],
+	// 		id: 'test' + i,
+	// 		tags: [['general', 'anime', 'suggestion', 'bug'][i % 4]],
+	// 		sticky: 0,
+	// 		created: (new Date()).toISOString()
+	// 	})
+	// }
+	// 
+	// threads[13].sticky = 1
 	
 	threads.sort((a, b) => {
-		return (a.sticky ? (-1 + b.sticky) : b.sticky) || (a.created > b.created) ? -1 : ((a.created < b.created) ? 1 : 0)
+		let order = (a.sticky ? (-1 + b.sticky) : b.sticky)
+		
+		if(order === 0)
+			return (a.created > b.created) ? -1 : ((a.created < b.created) ? 1 : 0)
+		
+		return order
 	})
+	
+	yield threads.map(thread => arn.set('Threads', thread.id, thread))
 	
 	if(threads.length > maxThreadCount)
 		threads.length = maxThreadCount
@@ -50,7 +64,14 @@ exports.get = function*(request, response) {
 	let idToUser = {}
 	
 	users.forEach(user => idToUser[user.id] = user)
-	threads.forEach(thread => thread.author = idToUser[thread.authorId])
+	threads.forEach(thread => {
+		thread.author = idToUser[thread.authorId]
+		
+		if(!thread.tags)
+			thread.icons = []
+		else
+			thread.icons = thread.tags.map(tag => tagToIcon[tag]).filter(icon => icon)
+	})
 	
 	response.render({
 		user,
