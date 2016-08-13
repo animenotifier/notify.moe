d58b9a8acd176374f0c7e6f2c551686d6f37baf6
Eduard Urbach <e.urbach@gmail.com>
2016-08-13 12:19:22 +0900

    Improved forum style

--- pages/forum/forum.js
+++ pages/forum/forum.js
@@ -1,8 +1,50 @@
-const maxThreadCount = 25
+const maxThreadCount = 13
 
 exports.get = function*(request, response) {
 	let user = request.user
-	let threads = yield arn.all('Threads')
+	let tag = request.params[0]
+	let threads = null
+	
+	if(!tag)
+		threads = yield arn.all('Threads')
+	else
+		threads = yield arn.filter('Threads', thread => thread.tags && thread.tags.indexOf(tag) !== -1)
+	
+	const testTexts = [
+		'A Guide to Forum Flags	',
+		'Guidelines	',
+		'Ban the User Above You #3 (with hashtags)	',
+		'Post a Random Fact About the User Above You #8 Electric Boogaloo	',
+		'Should There Be a Season Three of “Chuunibyou demo Koi ga Shitai”?	',
+		'Why would anyone vote for Trump?	',
+		'Count to 10k Thread V2	',
+		'Watched or Not Watched?	',
+		'Guess the Above User’s Gender	',
+		'Shutting Down Hummingbird	',
+		'Anime like Mouryou no Hako? new	',
+		'Any Overwatch PS4 Players? new	',
+		'Any anime inspired by NGE?	'
+	]
+	
+	for(let i = 0; i < 15; i++) {
+		threads.push({
+			title: testTexts[i % testTexts.length],
+			text: testTexts[i % testTexts.length],
+			authorId: ['HyhW-TsW', 'VkBaMJ6ux', 'NyGyZ2xwe', 'EkffWKXte'][i % 4],
+			id: 'test' + i,
+			sticky: false,
+			created: (new Date()).toISOString()
+		})
+	}
+	
+	threads[13].sticky = true
+	
+	threads.sort((a, b) => {
+		if(a.sticky !== b.sticky)
+			return a.sticky ? (-1 + b.sticky) : (1 - b.sticky)
+		
+		return (a.created > b.created) ? -1 : ((a.created < b.created) ? 1 : 0)
+	})
 	
 	if(threads.length > maxThreadCount)
 		threads.length = maxThreadCount
--- pages/threads/threads.js
+++ pages/threads/threads.js
@@ -12,8 +12,36 @@ exports.get = function*(request, response) {
 	let thread = yield arn.get('Threads', threadId)
 	yield arn.get('Users', thread.authorId).then(author => thread.author = author)
 	
+	let posts = yield arn.filter('Posts', post => post.threadId === threadId)
+	
+	const testTexts = [
+		'Lorem ipsum dolor sit amet',
+		'Lorem ipsum',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at suscipit enim. Proin nec magna non lacus molestie sollicitudin ornare eu risus. Praesent tincidunt sapien at est convallis, sit amet scelerisque nisl ultrices. Proin vitae turpis semper, efficitur neque et, pellentesque mauris. In dui nisl, elementum ultrices mollis a, pretium et enim. Quisque eget quam quis mauris ornare rutrum. Aliquam finibus metus eget magna dapibus, non porta nibh venenatis. Mauris eget ullamcorper lorem, id blandit magna. Integer non dui sed diam consequat viverra.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus at suscipit enim. Proin nec magna non lacus molestie sollicitudin ornare eu risus. Praesent tincidunt sapien at est convallis, sit amet scelerisque nisl ultrices. Proin vitae turpis semper, efficitur neque et, pellentesque mauris. In dui nisl, elementum ultrices mollis a, pretium et enim. Quisque eget quam quis mauris ornare rutrum. Aliquam finibus metus eget magna dapibus, non porta nibh venenatis. Mauris eget ullamcorper lorem, id blandit magna. Integer non dui sed diam consequat viverra.\n\nDonec porttitor elementum luctus. Fusce luctus, justo nec dictum rutrum, risus tortor maximus purus, finibus ornare enim erat ut urna. Nulla imperdiet quam nec sapien viverra, id maximus mi commodo. Maecenas ac vestibulum dolor, sit amet aliquam lectus. Integer ut nulla dapibus, porta ipsum et, pharetra mi. Suspendisse cursus, metus eu tempor venenatis, est augue varius eros, congue hendrerit tortor sapien vel elit. Etiam ornare mi eu neque molestie, ut mollis purus accumsan. Nunc vitae feugiat felis. Vivamus magna orci, molestie a justo at, finibus accumsan erat. Etiam hendrerit felis quis ligula aliquet venenatis. Nulla placerat, enim a fringilla tincidunt, felis nibh egestas risus, at elementum quam magna eget nunc. Maecenas nec sodales nisl. Vestibulum sollicitudin arcu id nunc gravida commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+	]
+	
+	for(let i = 0; i < 300; i++) {
+		posts.push({
+			text: testTexts[i % testTexts.length],
+			authorId: ['HyhW-TsW', 'VkBaMJ6ux', 'NyGyZ2xwe', 'EkffWKXte'][i % 4],
+			threadId,
+			id: 'test' + i,
+			likes: [],
+			created: (new Date()).toISOString()
+		})
+	}
+	
+	let users = yield arn.batchGet('Users', posts.map(post => post.authorId))
+	let idToUser = {}
+	
+	users.forEach(user => idToUser[user.id] = user)
+	posts.forEach(post => post.author = idToUser[post.authorId])
+	
 	response.render({
 		user,
-		thread
+		thread,
+		posts
 	})
 }
