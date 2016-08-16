e06f3d001013905ddc1ec015720fcbb3106b213d
Eduard Urbach <e.urbach@gmail.com>
2016-08-16 21:22:54 +0900

    Added error catch

--- startup/deserialize.js
+++ startup/deserialize.js
@@ -3,5 +3,5 @@ let passport = require('passport')
 // Deserialize
 // This means a web page is requesting full user data by some kind of hash.
 passport.deserializeUser(function(userId, done) {
-	return arn.get('Users', userId).then(user => done(undefined, user))
+	return arn.get('Users', userId).then(user => done(undefined, user)).catch(error => console.error('Deserialize error:', error))
 })
