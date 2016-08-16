ef352fc3d84f811c93ad08515d5fe55493a366bf
Eduard Urbach <e.urbach@gmail.com>
2016-08-16 16:39:47 +0900

    Fixed invisible avatars

--- scripts/avatars.js
+++ scripts/avatars.js
@@ -2,15 +2,19 @@ function updateAvatars() {
 	let images = document.querySelectorAll('.user-image')
 
 	for(var i = 0; i < images.length; ++i) {
-		let element = images[i]
+		let img = images[i]
 		
-		element.onload = function() {
-			this.style.opacity = 1.0
-		}
+		if(img.naturalWidth === 0) {
+			img.onload = function() {
+				this.style.opacity = 1.0
+			}
 
-		element.onerror = function() {
-			this.src = '/images/elements/no-gravatar.png'
-			this.style.opacity = 1.0
+			img.onerror = function() {
+				this.src = '/images/elements/no-gravatar.png'
+				this.style.opacity = 1.0
+			}
+		} else {
+			img.style.opacity = 1.0
 		}
 	}
 }
