75b98955939b93c1e1ac382aaf4df67c8c5fa547
Eduard Urbach <e.urbach@gmail.com>
2016-08-17 10:42:12 +0900

    New logout mechanism

--- startup/logout.js
+++ startup/logout.js
@@ -1,19 +1,7 @@
 // Logout
-app.get('/logout', function(req, res) {
-	if(req.session && req.session.destroy) {
-		req.session.destroy(function(err) {
-			if(err)
-				console.error('Session destroy error:', error)
-			
-			Promise.delay(1000).then(() => {
-				res.writeHead(302, {
-					'Location': '/'
-				})
-				res.end()
-			})
-		})
-	} else {
-		req.logout()
-		res.redirect('/')
-	}
+app.get('/logout', function(request, response) {
+	request.logout()
+	Promise.delay(500).then(() => {
+		response.redirect('/')
+	})
 })
