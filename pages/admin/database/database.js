71ea2bb6f7a76fb3c8fab41673e5d8a7cf4e177a
Eduard Urbach <e.urbach@gmail.com>
2016-08-17 12:28:08 +0900

    Improved admin database view

--- pages/admin/database/database.js
+++ pages/admin/database/database.js
@@ -1,12 +1,28 @@
+const prettyBytes = require('pretty-bytes')
+const setRegex = /\| "arn" \| "use-default"\s* \| (\d+)\s* \| \d+\s* \| "(\w+)"\s* \| (\d+)\s* \|/g
+
 exports.get = function*(request, response) {
 	if(!arn.auth(request, response, 'admin'))
 		return
 
 	let user = request.user
 	let statusText = yield arn.execute('aql -c \'show sets\'')
+	let sets = []
+	
+	let match = null
+	while(match = setRegex.exec(statusText)) {
+		sets.push({
+			name: match[2],
+			objects: parseInt(match[1]),
+			memoryUsage: parseInt(match[3])
+		})
+	}
+	
+	sets.sort((a, b) => a.name.localeCompare(b.name))
 
 	response.render({
 		user,
-		statusText
+		sets,
+		prettyBytes
 	})
 }
