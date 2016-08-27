c0a860912277c598ffb9909af7bf60b69cf9162c
Eduard Urbach <e.urbach@gmail.com>
2016-08-27 15:45:37 +0900

    Upgraded to Aero 1.6.0

--- index.js
+++ index.js
@@ -5,8 +5,5 @@ global.HTTP = require('http-status-codes')
 // For POST requests
 app.use(require('body-parser').json())
 
-// Run all startup modules
-require('fs').readdirSync('startup').forEach(mod => require('./startup/' + mod))
-
 // Start the server
 app.run()
