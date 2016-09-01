6c8763959256effc885b7514652e3d47b0c46274
Eduard Urbach <e.urbach@gmail.com>
2016-09-01 18:02:19 +0900

    User list sorting is now based on last activity

--- jobs/user-lists.js
+++ jobs/user-lists.js
@@ -4,17 +4,17 @@ let updateUserLists = coroutine(function*() {
 	let tasks = []
 	let allUsers = yield arn.filter('Users', user => arn.isActiveUser(user))
 	let usersWithAvatar = allUsers.filter(user => user.avatar !== '')
-	
+
 	console.log(`${allUsers.length} active users`)
-	
+
 	for(let orderBy of Object.keys(arn.userOrderBy)) {
 		let method = arn.userOrderBy[orderBy]
 		let categories = method.getCategories()
 		let addUser = method.addUser
 		let cacheKey = `users:${orderBy}`
-		
+
 		console.log(chalk.yellow('✖'), `Updating user list ${chalk.yellow(orderBy)}...`)
-		
+
 		let updateUserList = coroutine(function*() {
 			let userList = allowNoAvatars.indexOf(orderBy) !== -1 ? allUsers : usersWithAvatar
 			let users = yield Promise.filter(userList, user => addUser(user, categories))
@@ -25,13 +25,13 @@ let updateUserLists = coroutine(function*() {
 				category.sort((a, b) => {
 					let aVatar = a.avatar !== ''
 					let bVatar = b.avatar !== ''
-					
+
 					if(aVatar === bVatar)
-						return new Date(a.registered) - new Date(b.registered)
+						return new Date(b.lastView.date) - new Date(a.lastView.date)
 					else
 						return bVatar - aVatar
 				})
-				
+
 				// Reduce data size for the database
 				categories[categoryName] = category.map(user => {
 					return {
@@ -40,9 +40,9 @@ let updateUserLists = coroutine(function*() {
 					}
 				})
 			})
-			
+
 			console.log(chalk.green('✔'), `Finished updating user list ${chalk.yellow(orderBy)}`)
-			
+
 			return arn.set('Cache', cacheKey, {
 				categories
 			}).catch(error => {
@@ -53,9 +53,9 @@ let updateUserLists = coroutine(function*() {
 		yield updateUserList()
 		yield Promise.delay(100)
 	}
-	
+
 	yield tasks
-	
+
 	console.log(chalk.green('✔'), 'Finished updating all user lists')
 })
 
