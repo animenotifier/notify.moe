73a655b67fda4fee9328dd148ff26c1643b756c5
Eduard Urbach <e.urbach@gmail.com>
2016-09-01 17:14:52 +0900

    Added nick checks

---
+++ patches/nick-to-user-check.js
@@ -0,0 +1,23 @@
+let nicks = {}
+
+arn.db.ready.then(() => {
+	arn.forEach('Users', user => {
+		// arn.get('NickToUser', user.nick).then(record => {
+		// 	if(record.userId !== user.id)
+		// 		console.log(user.nick)
+		// })
+		if(nicks[user.nick]) {
+			console.log('Double nick: ' + user.nick)
+
+			if(user.accounts.google) {
+				arn.changeNick(user, 'g' + user.accounts.google)
+			} else if(user.accounts.facebook) {
+				arn.changeNick(user, 'fb' + user.accounts.facebook)
+			} else if(user.accounts.twitter) {
+				arn.changeNick(user, 't' + user.accounts.twitter)
+			}
+		}
+
+		nicks[user.nick] = user.id
+	})
+})
