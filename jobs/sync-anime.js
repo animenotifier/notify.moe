bcb504b331741fe69069eb38179892f6a4bb39ad
Eduard Urbach <e.urbach@gmail.com>
2016-09-03 10:15:16 +0900

    Reduced number of requests to Anilist

--- jobs/sync-anime.js
+++ jobs/sync-anime.js
@@ -2,27 +2,39 @@ const maxPage = 5
 
 let importAnimeFromAniList = coroutine(function*() {
 	console.log(chalk.yellow('✖'), 'Import anime from anilist...')
-	
+
 	// Get an access token
 	yield arn.listProviders.AniList.authorize()
-	
+
 	// Check the latest X pages for new anime edits
 	for(let page = 1; page <= maxPage; page++) {
 		yield Promise.delay(1200)
-		
+
 		// Get the list of 40 anime
 		let animeList = yield arn.listProviders.AniList.getAnimeFromPage(page)
-		
-		// Write the new anime data into the DB
-		yield animeList.map(anime => arn.set('Anime', anime.id, anime))
 
-		console.log(chalk.green('✔'), `Finished importing anilist page ${chalk.yellow(page)} (${animeList.length} anime)`)
+		for(let anime of animeList) {
+			let oldAnime = yield arn.get('Anime', anime.id).catch(error => null)
+
+			// Compare edit dates
+			if(!oldAnime || oldAnime.anilistEdited !== anime.anilistEdited) {
+				console.log(chalk.cyan('↻'), `Updating anime details: '${anime.title.romaji}'`)
+				let details = yield arn.listProviders.AniList.getAnimeDetails(anime.id)
+
+				// Write additional info to DB
+				yield arn.set('Anime', anime.id, Object.assign(anime, details))
+			} else {
+				yield arn.set('Anime', anime.id, anime)
+			}
+		}
+
+		console.log(chalk.green('✔'), `Finished checking anilist page ${chalk.yellow(page)} (${animeList.length} anime)`)
 	}
-	
+
 	// Update the anime list used for the background jobs process
 	arn.animeList = yield arn.all('Anime')
-	
-	console.log(chalk.green('✔'), `Finished importing basic anime data from AniList`)
+
+	console.log(chalk.green('✔'), `Finished syncing anime data with AniList`)
 })
 
 arn.repeatedly(1 * hours, importAnimeFromAniList)
