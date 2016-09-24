bc697377fb512c6bd46d488d723a52061bf39fe7
Eduard Urbach <e.urbach@gmail.com>
2016-09-24 14:57:47 +0900

    Maintenance

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 global.arn = require('./lib')
 global.app = require('aero')()
 global.HTTP = require('http-status-codes')
-
+arn.maintenance = true
 app.on('database ready', db => {
 	global.db = db
 })
