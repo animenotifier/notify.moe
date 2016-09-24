cc8f6ad2be9fe36af15eed0546ba743f9fe9a6ce
Eduard Urbach <e.urbach@gmail.com>
2016-09-24 14:18:56 +0900

    Disabled chat bot messages

--- startup/chatbot.js
+++ startup/chatbot.js
@@ -2,9 +2,9 @@ let dnode = require('dnode')
 
 arn.chatBot = {
 	sendMessage: function(channel, message) {
-		dnode.connect(app.config.ports.chatBot)
-		.on('remote', remote => remote.sendMessage(channel, message))
-		.on('fail', console.error)
-		.on('error', console.error)
+		// dnode.connect(app.config.ports.chatBot)
+		// .on('remote', remote => remote.sendMessage(channel, message))
+		// .on('fail', console.error)
+		// .on('error', console.error)
 	}
 }
