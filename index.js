7207b9cba09613a55612eb0443aa3d4c362b786a
Eduard Urbach <e.urbach@gmail.com>
2016-10-06 18:51:14 +0900

    Removed maintenance mode

--- index.js
+++ index.js
@@ -2,23 +2,12 @@ global.arn = require('./lib')
 global.app = require('aero')()
 global.HTTP = require('http-status-codes')
 
-if(app.production)
-	arn.maintenance = true
-
 app.on('database ready', db => {
 	global.db = db
 })
 
 // For POST requests
 app.use(require('body-parser').json())
-
-let chalk = require('chalk')
-app.use((request, response, next) => {
-	console.log(chalk.cyan(request.url))
-	console.log(request.params)
-	console.log(request.query)
-	next()
-})
 
 // Start the server
 app.run()
