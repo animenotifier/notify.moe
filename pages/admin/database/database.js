c5277bb1c83be24db37b041d620740b141f3e9d2
Eduard Urbach <e.urbach@gmail.com>
2016-11-01 18:40:45 +0900

    Fixed admin database page

--- pages/admin/database/database.js
+++ pages/admin/database/database.js
@@ -1,5 +1,5 @@
 const prettyBytes = require('pretty-bytes')
-const setRegex = /\| "arn" \| "use-default"\s* \| (\d+)\s* \| \d+\s* \| "(\w+)"\s* \| (\d+)\s* \|/g
+const setRegex = /\| "arn"\s* \| "use-default"\s* \| (\d+)\s* \| \d+\s* \| "(\w+)"\s* \| (\d+)\s* \|/g
 
 exports.get = function*(request, response) {
 	if(!arn.auth(request, response, 'admin'))
@@ -8,7 +8,7 @@ exports.get = function*(request, response) {
 	let user = request.user
 	let statusText = yield arn.execute('aql -c \'show sets\'')
 	let sets = []
-	
+
 	let match = null
 	while(match = setRegex.exec(statusText)) {
 		sets.push({
@@ -17,7 +17,7 @@ exports.get = function*(request, response) {
 			memoryUsage: parseInt(match[3])
 		})
 	}
-	
+
 	sets.sort((a, b) => a.name.localeCompare(b.name))
 
 	response.render({
