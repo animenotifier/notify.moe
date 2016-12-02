6372ff3d662a6235441745ed26f6350af8c0e690
Eduard Urbach <e.urbach@gmail.com>
2016-12-02 15:25:39 +0900

    New deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 #pack
 #go build
 scp notify.moe eduard@arn:~/beta/notify.moe.new
-ssh eduard@arn 'cd beta; killall notify.moe; rm notify.moe; mv notify.moe.new notify.moe; ./notify.moe &'
+ssh eduard@arn 'cd beta; killall notify.moe; rm notify.moe; mv notify.moe.new notify.moe; ./notify.moe > log.txt &'
