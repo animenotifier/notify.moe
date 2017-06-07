74b405e4b47e74869c3ddc67b5573bc9c4ecc205
Eduard Urbach <e.urbach@gmail.com>
2017-06-08 01:14:45 +0200

    Updated styles

--- pages/dashboard/dashboard.go
+++ pages/dashboard/dashboard.go
@@ -2,6 +2,8 @@ package dashboard
 
 import (
 	"github.com/aerogo/aero"
+	"github.com/animenotifier/arn"
+	"github.com/animenotifier/notify.moe/components"
 	"github.com/animenotifier/notify.moe/utils"
 )
 
@@ -9,24 +11,23 @@ const maxPosts = 5
 
 // Get ...
 func Get(ctx *aero.Context) string {
-	// posts, err := arn.GetPosts()
-
-	// if err != nil {
-	// 	return ctx.Error(500, "Error fetching posts", err)
-	// }
-
-	// arn.SortPostsLatestFirst(posts)
-
-	// if len(posts) > maxPosts {
-	// 	posts = posts[:maxPosts]
-	// }
-
-	// return ctx.HTML(components.Dashboard(posts))
 
 	user := utils.GetUser(ctx)
 
 	if user != nil {
-		return ctx.HTML("Welcome back, " + user.Nick + "!")
+		posts, err := arn.GetPosts()
+
+		if err != nil {
+			return ctx.Error(500, "Error fetching posts", err)
+		}
+
+		arn.SortPostsLatestFirst(posts)
+
+		if len(posts) > maxPosts {
+			posts = posts[:maxPosts]
+		}
+
+		return ctx.HTML(components.Dashboard(posts))
 	}
 
 	return ctx.HTML("ARN 4.0 is currently under construction.<br><a href='https://paypal.me/blitzprog' target='_blank' rel='noopener'>Support the development</a><br><a href='/auth/google'>Login via Google</a>")
