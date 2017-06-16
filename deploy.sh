2ed88947ecab02401e24646e7a2f882846bf13df
Eduard Urbach <e.urbach@gmail.com>
2017-06-17 01:36:44 +0200

    Cleanup

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#pack
-#go build
-scp notify.moe eduard@arn:~/beta/notify.moe.new
-ssh eduard@arn 'cd beta; killall notify.moe; rm notify.moe; mv notify.moe.new notify.moe; nohup ./notify.moe &'
