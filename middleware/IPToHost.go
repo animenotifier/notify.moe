5476753520d02cc878f7aa85500bbb8141f2a4d8
Eduard Urbach <e.urbach@gmail.com>
2017-06-23 15:57:58 +0200

    Removed unnecessary middleware

--- middleware/IPToHost.go
+++ middleware/IPToHost.go
@@ -4,20 +4,10 @@ import (
 	"net"
 	"time"
 
-	"github.com/aerogo/aero"
 	cache "github.com/patrickmn/go-cache"
 )
 
 var ipToHosts = cache.New(60*time.Minute, 30*time.Minute)
-
-// IPToHost middleware tries to find domain names for the given IP.
-func IPToHost() aero.Middleware {
-	return func(ctx *aero.Context, next func()) {
-		next()
-
-		go findHostsForIP(ctx.RealIP())
-	}
-}
 
 // GetHostsForIP returns all host names for the given IP (if cached).
 func GetHostsForIP(ip string) []string {
