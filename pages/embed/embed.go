092ba2ef0ab91347d3a2110dae75047c0e81fec5
Eduard Urbach <e.urbach@gmail.com>
2017-06-25 18:10:42 +0200

    Fixed embed error

--- pages/embed/embed.go
+++ pages/embed/embed.go
@@ -14,7 +14,7 @@ func Get(ctx *aero.Context) string {
 	user := utils.GetUser(ctx)
 
 	if user == nil {
-		return ctx.HTML(components.Login())
+		return utils.AllowEmbed(ctx, ctx.HTML(components.Login()))
 	}
 
 	animeList := user.AnimeList()
