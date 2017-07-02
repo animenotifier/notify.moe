771c16066aebcc92e2445f35d962865f9e32e654
Eduard Urbach <e.urbach@gmail.com>
2017-07-02 14:08:25 +0200

    Improved refresh

--- jobs/refresh-episodes/refresh-episodes.go
+++ jobs/refresh-episodes/refresh-episodes.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/animenotifier/arn"
 	"github.com/fatih/color"
 )
@@ -9,10 +13,18 @@ func main() {
 	color.Yellow("Refreshing episode information for each anime.")
 
 	for anime := range arn.MustStreamAnime() {
+		episodeCount := len(anime.Episodes)
+
 		err := anime.RefreshEpisodes()
 
 		if err != nil {
+			if strings.Contains(err.Error(), "missing a Shoboi ID") {
+				continue
+			}
+
 			color.Red(err.Error())
+		} else {
+			fmt.Println(anime.ID, "|", anime.Title.Canonical, "+"+strconv.Itoa(len(anime.Episodes)-episodeCount))
 		}
 	}
 
