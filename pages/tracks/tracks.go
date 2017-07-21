e9e0aa89eb9effb19350ab4b25dd787463863d08
Eduard Urbach <e.urbach@gmail.com>
2017-07-21 16:33:29 +0200

    Added Open Graph to soundtracks

--- pages/tracks/tracks.go
+++ pages/tracks/tracks.go
@@ -17,5 +17,15 @@ func Get(ctx *aero.Context) string {
 		return ctx.Error(http.StatusNotFound, "Track not found", err)
 	}
 
+	ctx.Data = &arn.OpenGraph{
+		Tags: map[string]string{
+			"og:title":     track.Media[0].Title,
+			"og:image":     track.Anime()[0].Image.Large,
+			"og:url":       "https://" + ctx.App.Config.Domain + track.Link(),
+			"og:site_name": "notify.moe",
+			"og:type":      "music.song",
+		},
+	}
+
 	return ctx.HTML(components.Track(track))
 }
