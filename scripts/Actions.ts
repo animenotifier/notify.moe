b45af0eaf90c3463fff699e887185c87e42fa42d
Eduard Urbach <e.urbach@gmail.com>
2017-10-01 08:06:43 +0200

    Added desktop app link to settings

--- scripts/Actions.ts
+++ scripts/Actions.ts
@@ -307,3 +307,8 @@ export function installExtension(arn: AnimeNotifier, button: HTMLElement) {
 	let browser: any = window["chrome"]
 	browser.webstore.install()
 }
+
+// Desktop app installation
+export function installApp() {
+	alert("Open your browser menu > 'More tools' > 'Add to desktop' and enable 'Open as window'.")
+}
