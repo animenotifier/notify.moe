8d07099cea51c13a665a96cb4bc3510dd1234332
Eduard Urbach <e.urbach@gmail.com>
2017-10-03 04:59:39 +0200

    HTML5 valid

---
+++ utils/EmptyImage.go
@@ -0,0 +1,6 @@
+package utils
+
+// EmptyImage returns the smallest possible 1x1 pixel image encoded in Base64.
+func EmptyImage() string {
+	return "data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+}
