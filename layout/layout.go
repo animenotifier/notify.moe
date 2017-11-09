454787b52d031523975220ed3f7e3619b3c75164
Eduard Urbach <e.urbach@gmail.com>
2017-11-09 08:45:51 +0100

    Deterministic output

--- layout/layout.go
+++ layout/layout.go
@@ -1,6 +1,8 @@
 package layout
 
 import (
+	"sort"
+
 	"github.com/aerogo/aero"
 	"github.com/animenotifier/arn"
 	"github.com/animenotifier/notify.moe/components"
@@ -11,5 +13,25 @@ import (
 func Render(ctx *aero.Context, content string) string {
 	user := utils.GetUser(ctx)
 	openGraph, _ := ctx.Data.(*arn.OpenGraph)
-	return components.Layout(ctx.App, ctx, user, openGraph, content)
+
+	// Make output order deterministic to profit from Aero caching.
+	// To do this, we need to create slices and sort the tags.
+	var meta []string
+	var tags []string
+
+	if openGraph != nil {
+		for name := range openGraph.Meta {
+			meta = append(meta, name)
+		}
+
+		sort.Strings(meta)
+
+		for name := range openGraph.Tags {
+			tags = append(tags, name)
+		}
+
+		sort.Strings(tags)
+	}
+
+	return components.Layout(ctx.App, ctx, user, openGraph, meta, tags, content)
 }
