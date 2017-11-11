2c37bcf6dd0e280a1004557f15cbb3ea6306f8c5
Eduard Urbach <e.urbach@gmail.com>
2017-11-11 12:50:01 +0100

    Removed benchmark

--- benchmarks/DB_AnimeList_test.go
+++ benchmarks/DB_AnimeList_test.go
@@ -6,21 +6,7 @@ import (
 	"github.com/animenotifier/arn"
 )
 
-func BenchmarkDBAnimeListGetMap(b *testing.B) {
-	user, _ := arn.GetUser("4J6qpK1ve")
-
-	b.ReportAllocs()
-	b.ResetTimer()
-
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			animeList, _ := arn.GetAnimeList(user.ID)
-			noop(animeList)
-		}
-	})
-}
-
-func BenchmarkDBAnimeListGet(b *testing.B) {
+func BenchmarkDatabaseGetAnimeList(b *testing.B) {
 	b.ReportAllocs()
 	b.ResetTimer()
 
