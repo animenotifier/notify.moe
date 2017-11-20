1f445feee413dc1effd5ea577759e005c5781fc2
Eduard Urbach <e.urbach@gmail.com>
2017-11-20 09:01:28 +0100

    Sort character anime by date

--- pages/character/character.go
+++ pages/character/character.go
@@ -2,6 +2,7 @@ package character
 
 import (
 	"net/http"
+	"sort"
 
 	"github.com/aerogo/aero"
 	"github.com/animenotifier/arn"
@@ -19,5 +20,19 @@ func Get(ctx *aero.Context) string {
 		return ctx.Error(http.StatusNotFound, "Character not found", err)
 	}
 
-	return ctx.HTML(components.CharacterDetails(character, user))
+	characterAnime := character.Anime()
+
+	sort.Slice(characterAnime, func(i, j int) bool {
+		if characterAnime[i].StartDate == "" {
+			return false
+		}
+
+		if characterAnime[j].StartDate == "" {
+			return true
+		}
+
+		return characterAnime[i].StartDate < characterAnime[j].StartDate
+	})
+
+	return ctx.HTML(components.CharacterDetails(character, characterAnime, user))
 }
