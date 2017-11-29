c53ab95e1ea304f5197bfcb986c0da8eb195b57b
Eduard Urbach <e.urbach@gmail.com>
2017-11-29 15:26:11 +0100

    Improved company list

--- pages/companies/companies.go
+++ pages/companies/companies.go
@@ -2,14 +2,13 @@ package companies
 
 import (
 	"sort"
+	"strings"
 
 	"github.com/aerogo/aero"
 	"github.com/animenotifier/arn"
 	"github.com/animenotifier/notify.moe/components"
 	"github.com/animenotifier/notify.moe/utils"
 )
-
-const maxEntries = 24
 
 // Get renders the companies page.
 func Get(ctx *aero.Context) string {
@@ -20,12 +19,25 @@ func Get(ctx *aero.Context) string {
 	})
 
 	sort.Slice(companies, func(i, j int) bool {
-		return companies[i].Created > companies[j].Created
+		return strings.ToLower(companies[i].Name.English) < strings.ToLower(companies[j].Name.English)
 	})
 
-	if len(companies) > maxEntries {
-		companies = companies[:maxEntries]
+	groups := [][]*arn.Company{}
+	currentGroupIndex := -1
+
+	previousFirstLetter := ""
+
+	for _, company := range companies {
+		firstLetter := strings.ToLower(company.Name.English[:1])
+
+		if firstLetter != previousFirstLetter {
+			groups = append(groups, []*arn.Company{})
+			currentGroupIndex++
+			previousFirstLetter = firstLetter
+		}
+
+		groups[currentGroupIndex] = append(groups[currentGroupIndex], company)
 	}
 
-	return ctx.HTML(components.Companies(companies, user))
+	return ctx.HTML(components.Companies(groups, user))
 }
