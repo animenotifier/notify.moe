3d1891e1a744d96ce62c7e5446886097a52136ff
Eduard Urbach <e.urbach@gmail.com>
2017-11-29 06:48:58 +0100

    Cleanup

---
+++ patches/import-mal-genres/import-mal-genres.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/animenotifier/arn"
+	"github.com/animenotifier/mal"
+	"github.com/fatih/color"
+)
+
+var malDB = arn.Node.Namespace("mal").RegisterTypes((*mal.Anime)(nil))
+
+func main() {
+	defer arn.Node.Close()
+	color.Yellow("Importing genres")
+
+	for anime := range arn.StreamAnime() {
+		malID := anime.GetMapping("myanimelist/anime")
+
+		if malID == "" {
+			continue
+		}
+
+		sync(anime, malID)
+	}
+
+	color.Green("Finished importing genres")
+}
+
+func sync(anime *arn.Anime, malID string) {
+	obj, err := malDB.Get("Anime", malID)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	malAnime := obj.(*mal.Anime)
+	anime.Genres = malAnime.Genres
+}
