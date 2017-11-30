f358ffbf7c4050c10f8f5a587553e86a0d12227c
Eduard Urbach <e.urbach@gmail.com>
2017-11-30 03:21:37 +0100

    Added genre overview

--- pages/genres/genres.go
+++ pages/genres/genres.go
@@ -4,11 +4,14 @@ import (
 	"github.com/aerogo/aero"
 	"github.com/animenotifier/arn"
 	"github.com/animenotifier/notify.moe/components"
+	"github.com/animenotifier/notify.moe/utils"
 )
 
 // Get ...
 func Get(ctx *aero.Context) string {
-	var genres []string
+	user := utils.GetUser(ctx)
+	genres := []string{}
+	genreToAnime := map[string]*arn.Anime{}
 
 	for _, genre := range arn.Genres {
 		if genre == "Hentai" {
@@ -18,5 +21,30 @@ func Get(ctx *aero.Context) string {
 		genres = append(genres, genre)
 	}
 
-	return ctx.HTML(components.Genres(genres))
+	allAnime := arn.AllAnime()
+	arn.SortAnimeByQuality(allAnime, "")
+
+	added := 0
+
+	for _, anime := range allAnime {
+		for _, genre := range anime.Genres {
+			// Skip genre that we don't care about
+			if !arn.Contains(genres, genre) {
+				continue
+			}
+
+			_, exists := genreToAnime[genre]
+
+			if !exists {
+				genreToAnime[genre] = anime
+				added++
+			}
+		}
+
+		if added >= len(genres) {
+			break
+		}
+	}
+
+	return ctx.HTML(components.Genres(genres, genreToAnime, user))
 }
