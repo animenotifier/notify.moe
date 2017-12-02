e9f15eacbcd6cbf65cffb1a57ec00f7ba8d4e7ab
Eduard Urbach <e.urbach@gmail.com>
2017-12-02 21:06:15 +0100

    Added hide anime in my collection button

--- scripts/Actions/Explore.ts
+++ scripts/Actions/Explore.ts
@@ -1,4 +1,5 @@
 import { AnimeNotifier } from "../AnimeNotifier"
+import { findAll } from "scripts/Utils";
 
 // Filter anime on explore page
 export function filterAnime(arn: AnimeNotifier, input: HTMLInputElement) {
@@ -7,4 +8,13 @@ export function filterAnime(arn: AnimeNotifier, input: HTMLInputElement) {
 	let type = arn.app.find("filter-type") as HTMLSelectElement
 
 	arn.app.load(`/explore/anime/${year.value}/${status.value}/${type.value}`)
+}
+
+// Hides anime that are already in your list.
+export function hideAddedAnime() {
+	for(let anime of findAll("anime-grid-cell")) {
+		if(anime.dataset.added === "true") {
+			anime.classList.toggle("anime-grid-cell-hide")
+		}
+	}
 }
