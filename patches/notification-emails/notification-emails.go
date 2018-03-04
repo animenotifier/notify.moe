057dab988ca33aa5f2e46e5b510e05a860264f3c
Eduard Urbach <e.urbach@gmail.com>
2018-03-04 15:43:00 +0100

    Minor changes

--- patches/notification-emails/notification-emails.go
+++ patches/notification-emails/notification-emails.go
@@ -10,7 +10,7 @@ func main() {
 	defer arn.Node.Close()
 
 	for user := range arn.StreamUsers() {
-		if user.Notifications().CountUnseen() <= 10 && !user.IsActive() && user.Email != "" && len(user.AnimeList().Items) > 0 {
+		if user.Notifications().CountUnseen() <= 10 && !user.IsActive() && user.Email != "" && len(user.AnimeList().Items) == 0 {
 			fmt.Println(user.Email)
 		}
 	}
