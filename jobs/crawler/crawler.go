13ad0b78da71dedb841a0cdde16980efef05b5df
Eduard Urbach <e.urbach@gmail.com>
2018-03-08 20:07:03 +0100

    Improved crawler

--- jobs/crawler/crawler.go
+++ jobs/crawler/crawler.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"time"
+
+	"github.com/animenotifier/arn"
+	"github.com/fatih/color"
 
 	"github.com/aerogo/crawler"
 )
@@ -9,12 +13,26 @@ import (
 const userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.166 Safari/537.36"
 
 func main() {
+	defer arn.Node.Close()
+
 	malCrawler := crawler.New(userAgent, 1*time.Second, 20000)
+	count := 0
 
-	malCrawler.Queue(&crawler.Task{
-		URL:         "https://github.com/animenotifier/notify.moe",
-		Destination: "file.html",
-	})
+	for anime := range arn.StreamAnime() {
+		malID := anime.GetMapping("myanimelist/anime")
 
+		if malID == "" {
+			continue
+		}
+
+		malCrawler.Queue(&crawler.Task{
+			URL:         "https://myanimelist.net/anime/" + malID,
+			Destination: fmt.Sprintf("mal/anime-%s.html", malID),
+		})
+
+		count++
+	}
+
+	color.Yellow("Queued up %d links", count)
 	malCrawler.Wait()
 }
