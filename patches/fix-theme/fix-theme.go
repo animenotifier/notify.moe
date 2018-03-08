93e848b2aae70e1df7ac3fc2b799c58b583a6775
Eduard Urbach <e.urbach@gmail.com>
2018-03-08 19:09:35 +0100

    Patches cleanup

--- patches/fix-theme/fix-theme.go
+++ patches/fix-theme/fix-theme.go
@@ -10,9 +10,7 @@ func main() {
 	for user := range arn.StreamUsers() {
 		settings := user.Settings()
 
-		if user.IsPro() {
-			settings.Theme = "dark"
-		} else {
+		if !user.IsPro() {
 			settings.Theme = "light"
 		}
 
