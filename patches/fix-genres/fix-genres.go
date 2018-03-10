d7517012dd30472d9add762a069da0dec90ce2fd
Eduard Urbach <e.urbach@gmail.com>
2018-03-10 18:52:46 +0100

    Added genre whitespace fix

---
+++ patches/fix-genres/fix-genres.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/animenotifier/arn"
+)
+
+func main() {
+	defer arn.Node.Close()
+
+	for anime := range arn.StreamAnime() {
+		for i := 0; i < len(anime.Genres); i++ {
+			old := anime.Genres[i]
+			genre := strings.TrimSpace(anime.Genres[i])
+
+			if genre != old {
+				fmt.Println(anime.Title.Canonical, genre)
+			}
+
+			anime.Genres[i] = genre
+		}
+
+		anime.Save()
+	}
+}
