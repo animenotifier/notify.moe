ed4e701eb7923505db5ac5bd23dc86e22f51b868
Scott <Soulcramer@gmail.com>
2018-03-14 16:09:32 +0100

    📝 Changed documentation

--- pages/profile/tracks.go
+++ pages/profile/tracks.go
@@ -25,7 +25,7 @@ func GetSoundTracksByUser(ctx *aero.Context) string {
 	// Fetch all eligible tracks
 	allTracks := fetchAllByUser(viewUser.ID)
 
-	// Sort the tracks by number of likes
+	// Sort the tracks by publication date
 	arn.SortSoundTracksLatestFirst(allTracks)
 
 	// Slice the part that we need
@@ -44,11 +44,11 @@ func GetSoundTracksByUser(ctx *aero.Context) string {
 	}
 
 	// Otherwise, send the full page
-	return ctx.HTML(components.TrackList(tracks,viewUser, nextIndex, user, ctx.URI()))
+	return ctx.HTML(components.TrackList(tracks, viewUser, nextIndex, user, ctx.URI()))
 
 }
 
-// GetSoundTracksLikedByUser shows all soundtracks of a particular user.
+// GetSoundTracksLikedByUser shows all soundtracks liked by a particular user.
 func GetSoundTracksLikedByUser(ctx *aero.Context) string {
 	nick := ctx.Get("nick")
 	user := utils.GetUser(ctx)
@@ -63,7 +63,7 @@ func GetSoundTracksLikedByUser(ctx *aero.Context) string {
 	// Fetch all eligible tracks
 	allTracks := fetchAllLikedByUser(viewUser.ID)
 
-	// Sort the tracks by number of likes
+	// Sort the tracks by publication date
 	arn.SortSoundTracksLatestFirst(allTracks)
 
 	// Slice the part that we need
@@ -82,5 +82,5 @@ func GetSoundTracksLikedByUser(ctx *aero.Context) string {
 	}
 
 	// Otherwise, send the full page
-	return ctx.HTML(components.TrackList(tracks,viewUser, nextIndex, user, ctx.URI()))
+	return ctx.HTML(components.TrackList(tracks, viewUser, nextIndex, user, ctx.URI()))
 }
