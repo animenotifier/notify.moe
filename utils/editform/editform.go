3ddfb73e881214bbecae91a26e777ccda64af94f
Eduard Urbach <e.urbach@gmail.com>
2018-03-15 17:01:00 +0100

    Added mountable class to edit buttons

--- utils/editform/editform.go
+++ utils/editform/editform.go
@@ -43,12 +43,12 @@ func Render(obj interface{}, title string, user *arn.User) string {
 			isDraft := v.FieldByName("IsDraft").Interface().(bool)
 
 			if isDraft {
-				b.WriteString(`<div class="buttons"><button class="action" data-action="publish" data-trigger="click">` + utils.Icon("share-alt") + `Publish</button></div>`)
+				b.WriteString(`<div class="buttons"><button class="mountable action" data-action="publish" data-trigger="click">` + utils.Icon("share-alt") + `Publish</button></div>`)
 			}
 		}
 
 		if user.Role == "editor" || user.Role == "admin" {
-			b.WriteString(`<button class="action" data-action="deleteObject" data-trigger="click" data-return-path="/` + lowerCaseTypeName + "s" + `" data-confirm-type="` + lowerCaseTypeName + `">` + utils.Icon("trash") + `Delete</button>`)
+			b.WriteString(`<button class="mountable action" data-action="deleteObject" data-trigger="click" data-return-path="/` + lowerCaseTypeName + "s" + `" data-confirm-type="` + lowerCaseTypeName + `">` + utils.Icon("trash") + `Delete</button>`)
 		}
 
 		b.WriteString(`</div>`)
