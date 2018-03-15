78c2172385be4b70661eb07be8172843f2e6b6b5
Eduard Urbach <e.urbach@gmail.com>
2018-03-16 00:38:04 +0100

    Added ratings reset tool

---
+++ patches/reset-animelist-ratings/reset-animelist-ratings.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/animenotifier/arn"
+)
+
+func main() {
+	defer arn.Node.Close()
+
+	animeList, err := arn.GetAnimeList("3eoFUr_zg")
+	arn.PanicOnError(err)
+
+	animeList.Lock()
+	for _, item := range animeList.Items {
+		item.Rating.Overall = 0
+	}
+	animeList.Unlock()
+
+	animeList.Save()
+}
