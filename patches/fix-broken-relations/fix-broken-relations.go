516b4a1966e6b5fb99421a49bfd50a711460c3de
Eduard Urbach <e.urbach@gmail.com>
2018-03-26 22:24:42 +0200

    Added a patch to fix broken relations

---
+++ patches/fix-broken-relations/fix-broken-relations.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/animenotifier/arn"
+	"github.com/fatih/color"
+)
+
+func main() {
+	color.Yellow("Fixing broken anime relations")
+
+	defer arn.Node.Close()
+
+	count := 0
+
+	for relations := range arn.StreamAnimeRelations() {
+		brokenIDs := []string{}
+
+		for _, item := range relations.Items {
+			_, err := arn.GetAnime(item.AnimeID)
+
+			if err != nil {
+				brokenIDs = append(brokenIDs, item.AnimeID)
+			}
+		}
+
+		for _, brokenID := range brokenIDs {
+			relations.Remove(brokenID)
+			count++
+		}
+
+		relations.Save()
+	}
+
+	color.Green("Finished removing %d broken relations.", count)
+}
