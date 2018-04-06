a8081d95045f469f1c3cff2c0d957f4096da23f6
Eduard Urbach <e.urbach@gmail.com>
2018-04-06 07:48:52 +0200

    Fix wrong IDs patch

---
+++ patches/fix-wrong-ids-in-animelists/fix-wrong-ids-in-animelists.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/animenotifier/arn"
+	"github.com/fatih/color"
+)
+
+func main() {
+	color.Yellow("Fixing anime IDs in anime lists")
+
+	defer color.Green("Finished")
+	defer arn.Node.Close()
+
+	finder := arn.NewAnimeFinder("kitsu/anime")
+
+	for animeList := range arn.StreamAnimeLists() {
+		modified := false
+
+		for _, item := range animeList.Items {
+			anime := item.Anime()
+
+			if anime != nil {
+				continue
+			}
+
+			anime = finder.GetAnime(item.AnimeID)
+
+			if anime != nil {
+				item.AnimeID = anime.ID
+				modified = true
+			}
+
+			fmt.Println(item.AnimeID, anime, animeList.User().Nick)
+		}
+
+		if modified {
+			animeList.Save()
+		}
+	}
+
+	time.Sleep(time.Second)
+}
