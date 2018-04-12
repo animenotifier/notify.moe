ea2e08d88db243fefae2c7dcfc853b1e9cf2f0ec
Eduard Urbach <e.urbach@gmail.com>
2018-04-12 23:05:24 +0200

    Improved API documentation

--- pages/apiview/api.go
+++ pages/apiview/api.go
@@ -1,22 +1,37 @@
 package apiview
 
 import (
+	"path"
 	"sort"
 
 	"github.com/aerogo/aero"
 	"github.com/animenotifier/arn"
+	"github.com/animenotifier/arn/autodocs"
 	"github.com/animenotifier/notify.moe/components"
+	"github.com/fatih/color"
 )
 
 // Get api page.
 func Get(ctx *aero.Context) string {
-	types := []string{}
+	types := []*autodocs.Type{}
 
 	for typeName := range arn.DB.Types() {
-		types = append(types, typeName)
+		if typeName == "Session" {
+			continue
+		}
+
+		typ, err := autodocs.GetTypeDocumentation(typeName, path.Join(arn.Root, "..", "arn", typeName+".go"))
+		types = append(types, typ)
+
+		if err != nil {
+			color.Red(err.Error())
+			continue
+		}
 	}
 
-	sort.Strings(types)
+	sort.Slice(types, func(i, j int) bool {
+		return types[i].Name < types[j].Name
+	})
 
 	return ctx.HTML(components.API(types))
 }
