ef41a1aa2651fa44ce368daa3c1a2154073c40d0
Eduard Urbach <e.urbach@gmail.com>
2018-04-16 17:54:34 +0200

    Improved AMV tags

--- pages/amv/opengraph.go
+++ pages/amv/opengraph.go
@@ -15,15 +15,8 @@ func getOpenGraph(ctx *aero.Context, amv *arn.AMV) *arn.OpenGraph {
 			"og:site_name": ctx.App.Config.Domain,
 			"og:type":      "video.other",
 		},
-		Meta: map[string]string{},
+		// Meta: map[string]string{},
 	}
-
-	// if amv.MainAnime() != nil {
-	// 	openGraph.Tags["og:image"] = amv.MainAnime().ImageLink("large")
-	// 	openGraph.Tags["og:description"] = amv.MainAnime().Title.Canonical + " (" + strings.Join(amv.Tags, ", ") + ")"
-	// } else {
-	// 	openGraph.Tags["og:description"] = strings.Join(amv.Tags, ", ")
-	// }
 
 	openGraph.Tags["og:description"] = strings.Join(amv.Tags, ", ")
 
@@ -33,11 +26,11 @@ func getOpenGraph(ctx *aero.Context, amv *arn.AMV) *arn.OpenGraph {
 		openGraph.Tags["og:video:width"] = "640"
 		openGraph.Tags["og:video:height"] = "360"
 
-		openGraph.Meta["twitter:player"] = openGraph.Tags["og:video"]
-		openGraph.Meta["twitter:player:width"] = openGraph.Tags["og:video:width"]
-		openGraph.Meta["twitter:player:height"] = openGraph.Tags["og:video:height"]
-		openGraph.Meta["twitter:player:stream"] = openGraph.Tags["og:video"]
-		openGraph.Meta["twitter:player:stream:content_type"] = openGraph.Tags["og:video:type"]
+		// openGraph.Meta["twitter:player"] = openGraph.Tags["og:video"]
+		// openGraph.Meta["twitter:player:width"] = openGraph.Tags["og:video:width"]
+		// openGraph.Meta["twitter:player:height"] = openGraph.Tags["og:video:height"]
+		// openGraph.Meta["twitter:player:stream"] = openGraph.Tags["og:video"]
+		// openGraph.Meta["twitter:player:stream:content_type"] = openGraph.Tags["og:video:type"]
 	}
 
 	return openGraph
