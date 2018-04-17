83d50548dc28f0b403f1849b4b56f14c5cc392f2
Eduard Urbach <e.urbach@gmail.com>
2018-04-17 13:16:53 +0200

    Improved uploader

--- scripts/Utils/fetchWithProgress.ts
+++ scripts/Utils/fetchWithProgress.ts
@@ -1,21 +1,18 @@
 export function fetchWithProgress(url, options: RequestInit, onProgress: ((this: XMLHttpRequest, ev: ProgressEvent) => any) | null): Promise<string> {
 	return new Promise((resolve, reject) => {
 		let xhr = new XMLHttpRequest()
+
+		xhr.addEventListener("load", e => resolve(xhr.responseText))
+		xhr.addEventListener("error", reject)
+
+		if(onProgress && xhr.upload) {
+			xhr.upload.addEventListener("progress", onProgress)
+		}
+
 		xhr.open(options.method || "GET", url)
 
 		for(let k in options.headers || {}) {
 			xhr.setRequestHeader(k, options.headers[k])
-		}
-
-		xhr.onload = e => resolve(xhr.responseText)
-		xhr.onerror = reject
-
-		if(onProgress) {
-			xhr.addEventListener("progress", onProgress)
-
-			if(xhr.upload) {
-				xhr.upload.onprogress = onProgress
-			}
 		}
 
 		xhr.send(options.body)
