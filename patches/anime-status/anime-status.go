af89e329c4fb3eb4461a74b8e58ab381a7acb874
Eduard Urbach <e.urbach@gmail.com>
2018-10-11 15:32:16 +0900

    Added anime status check

---
+++ patches/anime-status/anime-status.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/animenotifier/arn"
+	"github.com/fatih/color"
+)
+
+func main() {
+	color.Yellow("Checking anime status")
+
+	defer color.Green("Finished.")
+	defer arn.Node.Close()
+
+	for anime := range arn.StreamAnime() {
+		if anime.Status != anime.CalculatedStatus() {
+			fmt.Println("--------------------------------------------------------------------------------")
+			fmt.Printf("%s (%s)\n", anime.Title.Canonical, anime.Type)
+			fmt.Printf("%s => %s\n", color.RedString(anime.Status), color.YellowString(anime.CalculatedStatus()))
+			fmt.Println(anime.ID)
+		}
+	}
+}
