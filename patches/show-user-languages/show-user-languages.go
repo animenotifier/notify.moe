9b6d2db7f0bc24d9bf980df5c185c644029b9ef9
Eduard Urbach <e.urbach@gmail.com>
2018-10-28 12:46:55 +0900

    Added a patch to show languages

---
+++ patches/show-user-languages/show-user-languages.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/animenotifier/arn"
+)
+
+func main() {
+	languages := map[string]int{}
+
+	for user := range arn.StreamUsers() {
+		languages[user.Language]++
+	}
+
+	for language, users := range languages {
+		fmt.Printf("* [%s] %d users\n", language, users)
+	}
+}
