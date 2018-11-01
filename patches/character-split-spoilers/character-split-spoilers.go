257ba1bf590a8808b063d8c3aaf9c2e52ceb6d59
Eduard Urbach <e.urbach@gmail.com>
2018-11-01 15:17:03 +0900

    Added tool to split spoiler paragraphs

---
+++ patches/character-split-spoilers/character-split-spoilers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/animenotifier/arn"
+	"github.com/fatih/color"
+)
+
+func main() {
+	color.Yellow("Splitting character spoiler paragraphs into separate spoilers")
+
+	defer color.Green("Finished.")
+	defer arn.Node.Close()
+
+	for character := range arn.StreamCharacters() {
+		spoilers := character.Spoilers
+		finalSpoilers := []arn.Spoiler{}
+
+		for _, spoiler := range spoilers {
+			if !strings.Contains(strings.TrimSpace(spoiler.Text), "\n\n") {
+				finalSpoilers = append(finalSpoilers, spoiler)
+				continue
+			}
+
+			paragraphs := strings.Split(spoiler.Text, "\n\n")
+
+			for _, paragraph := range paragraphs {
+				finalSpoilers = append(finalSpoilers, arn.Spoiler{
+					Text: paragraph,
+				})
+			}
+		}
+
+		if len(finalSpoilers) != len(spoilers) {
+			character.Spoilers = finalSpoilers
+			character.Save()
+		}
+	}
+}
