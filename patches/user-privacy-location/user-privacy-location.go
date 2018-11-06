ba3a754d15da9bedef2a1b0e3184765bf006fb16
Eduard Urbach <e.urbach@gmail.com>
2018-11-06 19:45:03 +0900

    Added patch to reset privacy settings

---
+++ patches/user-privacy-location/user-privacy-location.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/animenotifier/arn"
+	"github.com/fatih/color"
+)
+
+func main() {
+	color.Yellow("Resetting location privacy to enabled")
+
+	defer color.Green("Finished.")
+	defer arn.Node.Close()
+
+	for user := range arn.StreamUsers() {
+		settings := user.Settings()
+		settings.Privacy.ShowLocation = true
+		settings.Save()
+	}
+}
