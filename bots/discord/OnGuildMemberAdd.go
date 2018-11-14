f023f540dd04f2934a4eb02fb84e7b5481d34d15
Eduard Urbach <e.urbach@gmail.com>
2018-11-14 10:40:21 +0900

    Updated welcome message

--- bots/discord/OnGuildMemberAdd.go
+++ bots/discord/OnGuildMemberAdd.go
@@ -20,7 +20,7 @@ func OnGuildMemberAdd(session *discordgo.Session, event *discordgo.GuildMemberAd
 	fmt.Println(event.Member.User.Username + " just joined!")
 
 	msg, err := session.ChannelMessageSend(welcomeChannel, fmt.Sprintf(
-		"**Welcome** %s!\n\nPlease introduce yourself in <#%s>.\n\n日本人は <#%s> で自己紹介して下さい！",
+		"**Welcome** %s!\n\nTo join this server, you need to verify your notify.moe account. Simply type `!verify` to receive instructions on how to do that.\n\nAfterwards, please introduce yourself in <#%s>.\n\n日本人は <#%s> で自己紹介して下さい！",
 		event.Member.User.Mention(),
 		enChannel,
 		jpChannel,
