719d41ed78c346c05eb9f68ff199fcff80a5a902
Eduard Urbach <e.urbach@gmail.com>
2018-11-14 10:45:56 +0900

    Updated welcome message

--- bots/discord/OnGuildMemberAdd.go
+++ bots/discord/OnGuildMemberAdd.go
@@ -7,9 +7,12 @@ import (
 )
 
 // Channels
-const welcomeChannel = "420600996268474368"
-const enChannel = "134910939140063232"
-const jpChannel = "334016470474424322"
+const (
+	welcomeChannel = "420600996268474368"
+	enChannel      = "134910939140063232"
+	jpChannel      = "334016470474424322"
+	botChannel     = "326743061793079296"
+)
 
 // Emoji
 const ramFive = ":ramFive:418520880511975426"
@@ -20,8 +23,9 @@ func OnGuildMemberAdd(session *discordgo.Session, event *discordgo.GuildMemberAd
 	fmt.Println(event.Member.User.Username + " just joined!")
 
 	msg, err := session.ChannelMessageSend(welcomeChannel, fmt.Sprintf(
-		"**Welcome** %s!\n\nTo join this server, you need to verify your notify.moe account. Simply type `!verify` to receive instructions on how to do that.\n\nAfterwards, please introduce yourself in <#%s>.\n\n日本人は <#%s> で自己紹介して下さい！",
+		"**Welcome** %s!\n\nTo join this server, you need to verify your notify.moe account. Simply type `!verify` in <#%s> to receive instructions on how to do that.\n\nAfterwards, please introduce yourself in <#%s>.\n\n日本人は <#%s> で自己紹介して下さい！",
 		event.Member.User.Mention(),
+		botChannel,
 		enChannel,
 		jpChannel,
 	))
