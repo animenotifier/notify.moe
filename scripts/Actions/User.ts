9ca6fd744f48588ea3b570662c155255c5cf16f5
Eduard Urbach <e.urbach@gmail.com>
2018-11-14 21:16:29 +0900

    Added show more button to profile tags

---
+++ scripts/Actions/User.ts
@@ -0,0 +1,35 @@
+import AnimeNotifier from "../AnimeNotifier"
+import Diff from "scripts/Diff";
+
+// Follow user
+export async function followUser(arn: AnimeNotifier, element: HTMLElement) {
+	try {
+		await arn.post(element.dataset.api)
+		await arn.reloadContent()
+		arn.statusMessage.showInfo("You are now following " + document.getElementById("nick").textContent + ".")
+	} catch(err) {
+		arn.statusMessage.showError(err)
+	}
+}
+
+// Unfollow user
+export async function unfollowUser(arn: AnimeNotifier, element: HTMLElement) {
+	try {
+		await arn.post(element.dataset.api)
+		await arn.reloadContent()
+		arn.statusMessage.showInfo("You stopped following " + document.getElementById("nick").textContent + ".")
+	} catch(err) {
+		arn.statusMessage.showError(err)
+	}
+}
+
+// Show more
+export function showMore(arn: AnimeNotifier, showMoreElement: HTMLElement) {
+	const elements = [...document.getElementsByClassName("show-more")]
+
+	for(let element of elements) {
+		Diff.mutations.queue(() => element.classList.remove("show-more"))
+	}
+
+	Diff.mutations.queue(() => showMoreElement.classList.add("show-more"))
+}
