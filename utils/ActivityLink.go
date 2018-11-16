1bd572cf3cb52e2d8422de00cfe8d2c2cba7fbf4
Eduard Urbach <e.urbach@gmail.com>
2018-11-17 05:33:40 +0900

    Started working on activity likes

---
+++ utils/ActivityLink.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/animenotifier/arn"
+)
+
+// ActivityAPILink returns the API link for any activity.
+func ActivityAPILink(activity arn.Activity) string {
+	id := activity.GetID()
+	typeName := activity.TypeName()
+
+	if activity.TypeName() == "ActivityCreate" {
+		created := activity.(*arn.ActivityCreate)
+		typeName = created.ObjectType
+		id = created.ObjectID
+	}
+
+	return fmt.Sprintf("/api/%s/%s", strings.ToLower(typeName), id)
+}
