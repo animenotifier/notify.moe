adb2f4a8332e87ee00013b3c64ff9739c527e576
Eduard Urbach <e.urbach@gmail.com>
2018-11-21 21:05:20 +0900

    Added patch for intros

---
+++ patches/user-show-intros/user-show-intros.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"regexp"
+
+	"github.com/animenotifier/arn"
+	"github.com/fatih/color"
+)
+
+var flaggedWords = regexp.MustCompile("fuck|fucking|freaking|shit|bad|terrible|awful|wtf")
+
+func main() {
+	color.Yellow("Showing user intros")
+
+	defer color.Green("Finished.")
+	defer arn.Node.Close()
+
+	for user := range arn.StreamUsers() {
+		if user.Introduction == "" {
+			continue
+		}
+
+		if flaggedWords.MatchString(user.Introduction) {
+			color.Cyan(user.Nick)
+			color.Red(user.Introduction)
+		}
+	}
+}
