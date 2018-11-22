545a2f219d8878952677cc371ead3c1a259800d5
Eduard Urbach <e.urbach@gmail.com>
2018-11-22 18:46:57 +0900

    Improved open graph data for groups

--- pages/group/opengraph.go
+++ pages/group/opengraph.go
@@ -8,9 +8,11 @@ import (
 func getOpenGraph(ctx *aero.Context, group *arn.Group) *arn.OpenGraph {
 	return &arn.OpenGraph{
 		Tags: map[string]string{
-			"og:title":     group.Name,
-			"og:url":       "https://" + ctx.App.Config.Domain + group.Link(),
-			"og:site_name": "notify.moe",
+			"og:title":       group.Name,
+			"og:description": group.Tagline,
+			"og:image":       "https:" + group.ImageLink("large"),
+			"og:url":         "https://" + ctx.App.Config.Domain + group.Link(),
+			"og:site_name":   "notify.moe",
 		},
 	}
 }
