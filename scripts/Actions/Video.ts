3502fdaf8d0853278e51dfc7bec31ce3b64481a1
Eduard Urbach <e.urbach@gmail.com>
2018-12-08 05:19:50 +0900

    Added video progress bar

--- scripts/Actions/Video.ts
+++ scripts/Actions/Video.ts
@@ -9,8 +9,28 @@ export function playVideo(arn: AnimeNotifier, video: HTMLVideoElement) {
 		return
 	}
 
-	video.addEventListener("loadeddata", () => {
+	video.addEventListener("canplay", () => {
 		togglePlayVideo(video)
+	})
+
+	let progressElement = video.parentElement.getElementsByClassName("video-current-progress")[0] as HTMLElement
+	let timeElement = video.parentElement.getElementsByClassName("video-time")[0]
+
+	video.addEventListener("timeupdate", () => {
+		let time = video.currentTime
+		let minutes = Math.trunc(time / 60)
+		let seconds = Math.trunc(time) % 60
+		let paddedSeconds = ("00" + seconds).slice(-2)
+		timeElement.textContent = `${minutes}:${paddedSeconds}`
+		progressElement.style.transform = `scaleX(${time / video.duration})`
+	})
+
+	video.addEventListener("waiting", () => {
+		arn.statusMessage.showInfo("Buffering...", -1)
+	})
+
+	video.addEventListener("playing", () => {
+		arn.statusMessage.close()
 	})
 
 	video.load()
