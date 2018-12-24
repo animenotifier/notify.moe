4f04a6c4942c516254ce7b0a05781a17ce6883a6
Eduard Urbach <e.urbach@gmail.com>
2018-12-24 16:25:12 +0900

    Added christmas notification

---
+++ patches/christmas-notification/christmas-notification.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/animenotifier/arn"
+	"github.com/fatih/color"
+)
+
+func main() {
+	color.Yellow("Sending notifications")
+
+	defer color.Green("Finished")
+	defer arn.Node.Close()
+
+	for user := range arn.StreamUsers() {
+		user.SendNotification(&arn.PushNotification{
+			Title:   "You have received a gift!",
+			Message: "Akyoto gifted you the item \"PRO Account - 1 month\".",
+			Icon:    "https://media.notify.moe/images/avatars/large/4J6qpK1ve.png?1545634334",
+			Link:    "https://notify.moe/thread/Sw0WDbsig",
+		})
+	}
+}
