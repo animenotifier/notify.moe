db7260965078e1053e63a6d1d3b7895d380c3487
Eduard Urbach <e.urbach@gmail.com>
2019-04-19 16:44:37 +0900

    Made change-role patch accept CLI arguments

--- patches/change-role/change-role.go
+++ patches/change-role/change-role.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/animenotifier/arn"
 )
+
+// Shell parameters
+var userID string
+var role string
+
+// Shell flags
+func init() {
+	flag.StringVar(&userID, "id", "", "ID of the user")
+	flag.StringVar(&role, "role", "", "The user's new role")
+	flag.Parse()
+}
 
 func main() {
 	defer arn.Node.Close()
 
-	user, _ := arn.GetUser("Vy2Hk5yvx")
-	user.Role = ""
+	// Get user
+	user, err := arn.GetUser(userID)
+	arn.PanicOnError(err)
+
+	// Save role
+	user.Role = role
 	user.Save()
 }
