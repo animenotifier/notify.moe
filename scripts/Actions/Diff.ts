70a62f06e5fb715e7788e248fdcf38f0e80d03d1
Eduard Urbach <e.urbach@gmail.com>
2019-04-22 15:05:16 +0900

    Fixed diffs

--- scripts/Actions/Diff.ts
+++ scripts/Actions/Diff.ts
@@ -3,13 +3,24 @@ import { requestIdleCallback } from "../Utils"
 
 // Load
 export function load(arn: AnimeNotifier, element: HTMLElement) {
-	let url = element.dataset.url || (element as HTMLAnchorElement).href
+	let url = element.dataset.url || (element as HTMLAnchorElement).getAttribute("href")
+
+	if(!url) {
+		arn.statusMessage.showError("Link doesn't have a target")
+		return
+	}
+
 	arn.app.load(url)
 }
 
 // Diff
 export async function diff(arn: AnimeNotifier, element: HTMLElement) {
-	let url = element.dataset.url || (element as HTMLAnchorElement).href
+	let url = element.dataset.url || (element as HTMLAnchorElement).getAttribute("href")
+
+	if(!url) {
+		arn.statusMessage.showError("Link doesn't have a target")
+		return
+	}
 
 	try {
 		await arn.diff(url)
