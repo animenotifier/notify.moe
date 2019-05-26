3094434326d30afd67a4f4368a46efd9f8a83a73
Eduard Urbach <e.urbach@gmail.com>
2019-05-26 11:56:41 +0900

    Simplified urlBase64ToUint8Array

--- scripts/PushManager.ts
+++ scripts/PushManager.ts
@@ -100,18 +100,12 @@ export default class PushManager {
 	}
 }
 
-function urlBase64ToUint8Array(base64String) {
+function urlBase64ToUint8Array(base64String: string) {
 	const padding = "=".repeat((4 - base64String.length % 4) % 4)
 	const base64 = (base64String + padding)
 	.replace(/\-/g, "+")
 	.replace(/_/g, "/")
 
 	const rawData = window.atob(base64)
-	const outputArray = new Uint8Array(rawData.length)
-
-	for(let i = 0; i < rawData.length; ++i) {
-		outputArray[i] = rawData.charCodeAt(i)
-	}
-
-	return outputArray
+	return Uint8Array.from([...rawData].map(char => char.charCodeAt(0)))
 }
