a72decf71434771f90452a837e3aa4395725885e
Eduard Urbach <e.urbach@gmail.com>
2019-05-26 12:00:10 +0900

    Allow data URIs for font loading

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func configure(app *aero.Application) *aero.Application {
 	// Content security policy
 	app.ContentSecurityPolicy.Set("img-src", "https: data:")
 	app.ContentSecurityPolicy.Set("connect-src", "https: wss: data:")
+	app.ContentSecurityPolicy.Set("font-src", "https: data:")
 
 	// Security
 	configureHTTPS(app)
