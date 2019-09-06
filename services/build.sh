712b9cdce125fe18b30835b7c39f4894076c9ba1
Eduard Urbach <e.urbach@gmail.com>
2019-09-06 13:43:53 +0900

    Added support for Go installed services

--- services/build.sh
+++ services/build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 INSTALLPATH="/etc/systemd/system/"
+GOBIN="$(go env GOPATH)/bin"
 cd $(dirname $0)
 
-for service in *; do
+for service in *.service; do
 	[ -f "$service" ] &&
 	echo "Installing services/$service" &&
 	sudo cp "services/$service" "$INSTALLPATH/$service" &&
 	sudo sed -i "s|MAKEFILE_USER|$USER|g" "$INSTALLPATH/$service" &&
+	sudo sed -i "s|MAKEFILE_GOBIN|$GOBIN|g" "$INSTALLPATH/$service" &&
 	sudo sed -i "s|MAKEFILE_PWD|$PWD|g" "$INSTALLPATH/$service"
 done
 
-sudo systemctl daemon-reload
+sudo systemctl daemon-reload &&
 echo -e "\nYou can now start the service using:\n\nsudo systemctl start animenotifier.service"
