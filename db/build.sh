6bfe81fdc32dc1933d4077111a484813197529cc
Eduard Urbach <e.urbach@gmail.com>
2019-09-09 08:27:32 +0900

    Fixed test script

--- db/build.sh
+++ db/build.sh
@@ -2,10 +2,10 @@
 TYPES=`curl -sS 'https://notify.moe/api/types' | jq '.[]' | cut -d '"' -f 2`
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd "$SCRIPTPATH"
-mkdir -p arn2
+mkdir -p arn
 
 for TYPENAME in $TYPES; do
 	echo "Downloading db/arn/$TYPENAME.dat"
 	URL=https://notify.moe/api/types/$TYPENAME/download
-	curl -sS $URL -o arn2/$TYPENAME.dat
+	curl -sS $URL -o arn/$TYPENAME.dat
 done
