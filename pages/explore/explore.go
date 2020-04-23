2181f3251acde6b2e1720f7d34744b41598b20b9
Eduard Urbach <e.urbach@gmail.com>
2020-04-23 17:38:28 +0900

    Started working on new explore page

--- pages/explore/explore.go
+++ pages/explore/explore.go
@@ -1,93 +1,13 @@
 package explore
 
 import (
-	"strconv"
-	"time"
-
 	"github.com/aerogo/aero"
 	"github.com/animenotifier/notify.moe/arn"
 	"github.com/animenotifier/notify.moe/components"
 )
 
-// Filter filters the anime for the explore page.
-func Filter(ctx aero.Context) error {
-	year := ctx.Get("year")
-	season := ctx.Get("season")
-	status := ctx.Get("status")
-	typ := ctx.Get("type")
+// Get the explore page.
+func Get(ctx aero.Context) error {
 	user := arn.GetUserFromContext(ctx)
-	now := time.Now()
-
-	if year == "" {
-		year = strconv.Itoa(now.Year())
-	}
-
-	if season == "" {
-		season = arn.DateToSeason(now)
-	}
-
-	if status == "" {
-		status = "current"
-	}
-
-	if typ == "" {
-		typ = "tv"
-	}
-
-	results := filterAnime(year, season, status, typ)
-
-	if year == "any" {
-		year = ""
-	}
-
-	if season == "any" {
-		season = ""
-	}
-
-	if status == "any" {
-		status = ""
-	}
-
-	if typ == "any" {
-		typ = ""
-	}
-
-	return ctx.HTML(components.ExploreAnime(results, year, season, status, typ, user))
-}
-
-func filterAnime(year, season, status, typ string) []*arn.Anime {
-	results := make([]*arn.Anime, 0, 1024)
-
-	for anime := range arn.StreamAnime() {
-		if anime.IsDraft {
-			continue
-		}
-
-		if year != "any" {
-			if len(anime.StartDate) < 4 {
-				continue
-			}
-
-			if anime.StartDate[:4] != year {
-				continue
-			}
-		}
-
-		if status != "any" && anime.Status != status {
-			continue
-		}
-
-		if season != "any" && anime.Season() != season {
-			continue
-		}
-
-		if (typ != "any" || anime.Type == "music" || anime.Type == "tba") && anime.Type != typ {
-			continue
-		}
-
-		results = append(results, anime)
-	}
-
-	arn.SortAnimeByQualityDetailed(results, status)
-	return results
+	return ctx.HTML(components.Explore(user))
 }
--- pages/index/exploreroutes/exploreroutes.go
+++ pages/index/exploreroutes/exploreroutes.go
@@ -3,6 +3,7 @@ package exploreroutes
 import (
 	"github.com/aerogo/aero"
 	"github.com/animenotifier/notify.moe/pages/explore"
+	"github.com/animenotifier/notify.moe/pages/explore/exploreanime"
 	"github.com/animenotifier/notify.moe/pages/explore/explorecolor"
 	"github.com/animenotifier/notify.moe/pages/explore/halloffame"
 	"github.com/animenotifier/notify.moe/utils/page"
@@ -11,8 +12,9 @@ import (
 // Register registers the page routes.
 func Register(app *aero.Application) {
 	// Explore
-	page.Get(app, "/explore", explore.Filter)
-	page.Get(app, "/explore/anime/:year/:season/:status/:type", explore.Filter)
+	page.Get(app, "/explore", explore.Get)
+	page.Get(app, "/explore/anime", exploreanime.Filter)
+	page.Get(app, "/explore/anime/:year/:season/:status/:type", exploreanime.Filter)
 	page.Get(app, "/explore/color/:color/anime", explorecolor.AnimeByAverageColor)
 	page.Get(app, "/explore/color/:color/anime/from/:index", explorecolor.AnimeByAverageColor)
 	page.Get(app, "/halloffame", halloffame.Get)
