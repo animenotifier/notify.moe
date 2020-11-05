a4479945b5ad56b0b21bb8c6cf63b98d42a88ee3
tomGER <github@tomger.eu>
2020-11-05 11:47:42 +0000

    Start work on AnimeList updating [Will break CI]

--- server/tokenapi/AnimeList.go
+++ server/tokenapi/AnimeList.go
@@ -13,6 +13,7 @@ type AnimeParameters struct {
 	Rating  arn.AnimeRatingCount
 }
 
+// AnimeUpdate parses parameters out of the JSON and then tries to integrate them into the database
 func AnimeUpdate(request *TokenRequest) error {
 	parameters := &AnimeParameters{}
 	animeJSON := request.JSON.Get("anime")
@@ -30,6 +31,19 @@ func AnimeUpdate(request *TokenRequest) error {
 	parameters.Rating.Story = int(rating.Get("story").Int())
 	parameters.Rating.Visuals = int(rating.Get("visuals").Int())
 	parameters.Rating.Soundtrack = int(rating.Get("soundtrack").Int())
+
+	animeList := request.User.AnimeList()
+
+	if !animeList.Contains(parameters.ID) {
+		err := animeList.Add(parameters.ID)
+		if err != nil {
+			return err
+		}
+	}
+
+	animeEntry := animeList.Find(parameters.ID)
+	lastEpisode := animeEntry.Episodes
+	// @TODO: Continue working here. See AnimeListItemAPI.go for similar implementation.
 
 	return nil
 }
