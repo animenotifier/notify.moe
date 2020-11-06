8bd56db75700ff012a5c258c7b26cba88abb4623
Eduard Urbach <e.urbach@gmail.com>
2020-11-07 06:37:41 +0900

    Removed outdated history render

--- pages/anime/editanime/history.go
+++ pages/anime/editanime/history.go
@@ -7,7 +7,7 @@ import (
 )
 
 // History of the edits.
-var History = history.Handler(renderHistory, "Anime", "AnimeCharacters", "AnimeRelations", "AnimeEpisodes")
+var History = history.Handler(renderHistory, "Anime", "AnimeCharacters", "AnimeRelations")
 
 func renderHistory(obj interface{}, entries []*arn.EditLogEntry, user *arn.User) string {
 	return components.EditAnimeTabs(obj.(*arn.Anime)) + components.EditLog(entries, user)
