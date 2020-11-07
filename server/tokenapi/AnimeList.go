f1dc5d99f5fd913fc2b8079b936f0a4da5c4f880
tumGER <tumger@outlook.de>
2020-11-07 16:56:20 +0100

    Reimplement tokenapi.AnimeUpdate() using animeListEntry struct

--- server/tokenapi/AnimeList.go
+++ server/tokenapi/AnimeList.go
@@ -6,44 +6,30 @@ import (
 	"github.com/animenotifier/notify.moe/arn"
 )
 
-type AnimeParameters struct {
-	Name    string
-	ID      string
-	Episode int
-	Rating  arn.AnimeRatingCount
-}
-
-// AnimeUpdate parses parameters out of the JSON and then tries to integrate them into the database
+// AnimeUpdate parses animeListEntry.out of the JSON and then tries to integrate them into the database
 func AnimeUpdate(request *TokenRequest) error {
-	parameters := &AnimeParameters{}
+	animeListEntry := &arn.AnimeListItem{}
 	animeJSON := request.JSON.Get("anime")
 
-	parameters.Name = animeJSON.Get("name").String()
-	parameters.ID = animeJSON.Get("id").String()
-	parameters.Episode = int(animeJSON.Get("episode").Int())
+	animeListEntry.Status = animeJSON.Get("status").String()
+	animeListEntry.AnimeID = animeJSON.Get("id").String()
+	animeListEntry.Episodes = int(animeJSON.Get("episode").Int())
+	animeListEntry.Notes = animeJSON.Get("notes").String()
+	animeListEntry.RewatchCount = int(animeJSON.Get("rewatchCount").Int())
+	animeListEntry.Private = animeJSON.Get("isPrivate").Bool()
 
-	if parameters.Name == "" && parameters.ID == "" {
-		return errors.New("Neither ID nor Name of the anime has been supplied")
+	if animeListEntry.AnimeID == "" {
+		return errors.New("No anime ID has been supplied")
 	}
 
 	rating := animeJSON.Get("ratings")
-	parameters.Rating.Overall = int(rating.Get("overall").Int())
-	parameters.Rating.Story = int(rating.Get("story").Int())
-	parameters.Rating.Visuals = int(rating.Get("visuals").Int())
-	parameters.Rating.Soundtrack = int(rating.Get("soundtrack").Int())
+	animeListEntry.Rating.Overall = float64(rating.Get("overall").Float())
+	animeListEntry.Rating.Story = float64(rating.Get("story").Float())
+	animeListEntry.Rating.Visuals = float64(rating.Get("visuals").Float())
+	animeListEntry.Rating.Soundtrack = float64(rating.Get("soundtrack").Float())
 
 	animeList := request.User.AnimeList()
-
-	if !animeList.Contains(parameters.ID) {
-		err := animeList.Add(parameters.ID)
-		if err != nil {
-			return err
-		}
-	}
-
-	animeEntry := animeList.Find(parameters.ID)
-	lastEpisode := animeEntry.Episodes
-	// @TODO: Continue working here. See AnimeListItemAPI.go for similar implementation.
+	animeList.Import(animeListEntry)
 
 	return nil
 }
