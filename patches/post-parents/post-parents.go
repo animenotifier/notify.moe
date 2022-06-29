1a65f8dcdf133b6660d6213310d4f0e448845206
Eduard Urbach <e.urbach@gmail.com>
2022-06-29 11:53:52 +0900

    Improved post parent check

--- patches/post-parents/post-parents.go
+++ patches/post-parents/post-parents.go
@@ -9,7 +9,13 @@ func main() {
 	defer arn.Node.Close()
 
 	for post := range arn.StreamPosts() {
-		if post.Parent() == nil {
+		if post.ParentID == "" {
+			continue
+		}
+
+		obj, _ := arn.DB.Get(post.ParentType, post.ParentID)
+
+		if obj == nil {
 			color.Yellow(post.ID)
 			color.Red(post.Text)
 		}
